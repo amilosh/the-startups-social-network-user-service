fbd3070f9ca16bae8f93b9006bd0ad7c4c3c8522
Chibix <chibisovanastena@gmail.com>
2024-10-26 19:47:45 +0300

    BJS2-27577 - Совместная работа над целью Внесла исправления после ревью

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.GoalInvitationDto;
 import school.faang.user_service.dto.SuccessResponse;
+import school.faang.user_service.dto.GoalInvitationResponseDto;
 import school.faang.user_service.entity.goal.GoalInvitation;
 import school.faang.user_service.service.GoalInvitationService;
 import school.faang.user_service.dto.goal.GoalInvitationFilterDto;
@@ -17,21 +18,24 @@ import java.util.List;
 public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
-    @PostMapping("/create")
+    @PutMapping("/create")
     public GoalInvitationDto createInvitation(@RequestBody GoalInvitationDto invitationDTO) {
         return goalInvitationService.createInvitation(invitationDTO);
     }
 
     @PutMapping("/accept/id")
-    public ResponseEntity<SuccessResponse> acceptGoalInvitation(@PathVariable long id) {
-        goalInvitationService.acceptGoalInvitation(id);
-        return ResponseEntity.ok(new SuccessResponse("Приглашение принято."));
+    public GoalInvitationResponseDto acceptGoalInvitation(@PathVariable long id) {
+        return goalInvitationService.acceptGoalInvitation(id);
     }
 
     @DeleteMapping("/reject/id")
-    public ResponseEntity<SuccessResponse> rejectGoalInvitation(@PathVariable long id) {
-        goalInvitationService.rejectGoalInvitation(id);
-        return ResponseEntity.ok(new SuccessResponse("Приглашение отклонено."));
+    public SuccessResponse rejectGoalInvitation(@PathVariable long id) {
+        if(goalInvitationService.rejectGoalInvitation(id)) {
+            return new SuccessResponse("Invitation declined.");
+        } else {
+            throw new IllegalArgumentException("Invitation does not exist.");
+        }
+
     }
 
     @PostMapping("/getInvitations")
--- src/main/java/school/faang/user_service/service/GoalInvitationService.java
+++ src/main/java/school/faang/user_service/service/GoalInvitationService.java
@@ -3,9 +3,11 @@ package school.faang.user_service.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.GoalInvitationDto;
+import school.faang.user_service.dto.GoalInvitationResponseDto;
 import school.faang.user_service.filter.GoalInvitationFilter;
 import school.faang.user_service.entity.goal.GoalInvitation;
 import school.faang.user_service.mapper.GoalInvitationMapper;
+import school.faang.user_service.mapper.GoalInvitationResponseMapper;
 import school.faang.user_service.repository.goal.GoalInvitationRepository;
 import school.faang.user_service.dto.goal.GoalInvitationFilterDto;
 
@@ -18,6 +20,7 @@ import java.util.stream.Stream;
 public class GoalInvitationService {
     private final GoalInvitationRepository goalInvitationRepository;
     private final GoalInvitationMapper goalInvitationMapper;
+    private final GoalInvitationResponseMapper goalInvitationResponseMapper;
     private final List<GoalInvitationFilter> goalnvitationFilterList;
 
     public GoalInvitationDto createInvitation(GoalInvitationDto invitationDTO) {
@@ -27,23 +30,27 @@ public class GoalInvitationService {
         return goalInvitationMapper.toInvitationDTO(invitation);
     }
 
-    public void acceptGoalInvitation(long id) {
+    public GoalInvitationResponseDto acceptGoalInvitation(long id) {
         GoalInvitation invitation = goalInvitationRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Приглашение не найдено."));
+                .orElseThrow(() -> new IllegalArgumentException("Invitation not found."));
         List<GoalInvitation> listInvitations = invitation.getInvited().getReceivedGoalInvitations();
         if (listInvitations.size() > 2) {
-            throw new IllegalArgumentException("Нельзя иметь больше 3 активных целей.");
+            throw new IllegalArgumentException("You cannot have more than 3 active goals.");
         }
         if (listInvitations.contains(id)) {
-            throw new IllegalArgumentException("У вас уже есть такая цель.");
+            throw new IllegalArgumentException("You already have such a goal.");
         }
         listInvitations.add(invitation);
+        return goalInvitationResponseMapper.toInvitationDTO(invitation);
     }
 
-    public void rejectGoalInvitation(long id) {
+    public boolean rejectGoalInvitation(long id) {
         if (goalInvitationRepository.existsById(id)) {
             goalInvitationRepository.deleteById(id);
-        } else throw new IllegalArgumentException("Приглашение отклонено.");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public List<GoalInvitation> getInvitationsByFilter(GoalInvitationFilterDto goalInvitationFilterDto) {
@@ -56,10 +63,10 @@ public class GoalInvitationService {
 
     public void createInvitationValidation(GoalInvitationDto invitationDTO) {
         if (invitationDTO.getInviterId().equals(invitationDTO.getInvitedUserId())) {
-            throw new IllegalArgumentException("Приглашающий и приглашенный - один и тот же человек");
+            throw new IllegalArgumentException("The inviter and the invitee are the same person.");
         }
         if (invitationDTO.getInviterId() == null || invitationDTO.getInvitedUserId() == null) {
-            throw new IllegalArgumentException("Пользователь не существует");
+            throw new IllegalArgumentException("User does not exist.");
         }
     }
 }
