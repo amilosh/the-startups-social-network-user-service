1c89163703cc04693d44cb308eb9ca6627ec688c
“Andrey <aiegorovceo@yandex.ru>
2024-10-29 18:05:18 +0300

    refactor(controller): refactor createGoal method in GoalController

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.controller.goal;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 import java.util.Optional;
 import school.faang.user_service.service.goal.GoalService;
 import school.faang.user_service.entity.goal.Goal;
@@ -17,24 +20,16 @@ public class GoalController {
 
     private final GoalService goalService;
 
-    // Внедрение зависимости GoalService с помощью конструктора
     @Autowired
     public GoalController(GoalService goalService) {
         this.goalService = goalService;
     }
 
-    /**
-     * Метод, обрабатывающий создание новой цели для пользователя
-     */
-    @PostMapping("/create")
-    public String createGoal(@RequestParam Long userId, @RequestBody Goal goal) {
-        // Проверка на наличие названия цели
-        if (goal.getTitle() == null || goal.getTitle().isEmpty()) {
-            return "Error: Title is required.";
-        }
+    @PostMapping
+    public ResponseEntity<Goal> createGoal(@RequestParam Long userId, @RequestBody Goal goal, @RequestParam List<Long> skillIds) {
+        Optional<Goal> createdGoal = goalService.createGoal(userId, goal, skillIds);
 
-        // Вызов метода createGoal в GoalService и обработка результата
-        Optional<Goal> createdGoal = goalService.createGoal(userId, goal);
-        return createdGoal.isPresent() ? "Goal created successfully." : "Error: Goal could not be created.";
+        return createdGoal.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.badRequest().build());
     }
 }
