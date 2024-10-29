4ebd63b452a4bbed9af57db4a415ce08606772e9
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-29 16:14:18 +0700

    implement goal controller

---
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -0,0 +1,46 @@
+package school.faang.user_service.controller.goal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.service.goal.GoalService;
+
+@RestController
+@RequestMapping("/goals")
+@RequiredArgsConstructor
+public class GoalController {
+
+    private final GoalService goalService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<Void> createGoal(@PathVariable Long userId, @RequestBody GoalDto goalDto) {
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{goalId}")
+                        .buildAndExpand(goalService.createGoal(userId, goalDto).getId())
+                        .toUri()
+        ).build();
+    }
+
+    @PutMapping("/{goalId}")
+    public ResponseEntity<Void> updateGoal(@PathVariable("goalId") Long goalId, @RequestBody GoalDto goalDto) {
+        goalService.updateGoal(goalId, goalDto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{goalId}")
+    public ResponseEntity<Void> deleteGoal(@PathVariable("goalId") Long goalId) {
+        goalService.deleteGoal(goalId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -0,0 +1,19 @@
+package school.faang.user_service.service.goal;
+
+import school.faang.user_service.dto.goal.GoalDto;
+import school.faang.user_service.dto.goal.GoalFilterDto;
+
+import java.util.List;
+
+public interface GoalService {
+
+    GoalDto createGoal(Long userId, GoalDto goalDto);
+
+    GoalDto updateGoal(Long goalId, GoalDto goalDto);
+
+    void deleteGoal(Long goalId);
+
+    List<GoalDto> getGoalsByUserId(Long userId, GoalFilterDto filter);
+
+    List<GoalDto> findSubtasksByGoalId(Long goalId);
+}
