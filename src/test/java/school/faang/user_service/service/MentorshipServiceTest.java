42bd05a8c6bcf30c399c02e4b505e5b7ac897db8
Roma Nifontov <romannif@mail.ru>
2024-10-29 15:37:27 +0500

    коммит3

--- src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -1,11 +1,14 @@
 package school.faang.user_service.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,13 +17,41 @@ import static org.mockito.Mockito.when;
 
 public class MentorshipServiceTest {
 
-    private final long NON_EXIST_USER_ID = 0L;
-
     @InjectMocks
     private MentorshipService service;
 
     @Mock
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    private final long NON_EXIST_USER_ID = 0L;
+    private final long EXISTING_USER_ID = 2L;
+    private User correctUser;
+    private User incorrectUser;
+    private User nonExistUser;
+
+    @BeforeEach
+    void initData() {
+        correctUser = User.builder()
+                .id(EXISTING_USER_ID)
+                .username("Roma")
+                .email("roma@mail.ru")
+                .city("Екатеринбург")
+                .mentees(new ArrayList<>())
+                .build();
+        nonExistUser = User.builder()
+                .id(NON_EXIST_USER_ID)
+                .username("Roma")
+                .email("roma@mail.ru")
+                .city("Екатеринбург")
+                .build();
+
+
+
+
+    }
+
+
+
 
     @Test
     public void testGetMenteesWithNoMenteesForMentor() {
