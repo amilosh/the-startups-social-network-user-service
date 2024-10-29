6871905baef3784d9fe9e48d619f49eb6ed1ea78
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-29 10:54:05 -0700

    fix: add an exception message and stack trace to error logs

--- src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
@@ -18,35 +18,35 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GoalInvitationValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleGoalInvitationValidation(GoalInvitationValidationException e) {
-        log.error("Goal Invitation Validation Error");
+        log.error("Goal Invitation Validation Error: {}", e.getMessage(), e);
         return new ErrorResponse("Goal Invitation Validation Error", e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleEntityNotFound(EntityNotFoundException e) {
-        log.error("Entity Not Found");
+        log.error("Entity Not Found: {}", e.getMessage(), e);
         return new ErrorResponse("Entity Not Found", e.getMessage());
     }
 
     @ExceptionHandler(DuplicateObjectException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleDuplicateObject(DuplicateObjectException e) {
-        log.error("Duplicated object");
+        log.error("Duplicated object: {}", e.getMessage(), e);
         return new ErrorResponse("Duplicated object", e.getMessage());
     }
 
     @ExceptionHandler(GoalInvitationStatusException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleGoalInvitationStatus(GoalInvitationStatusException e) {
-        log.error("Goal Invitation Status Error");
+        log.error("Goal Invitation Status Error: {}", e.getMessage(), e);
         return new ErrorResponse("Goal Invitation Status Error", e.getMessage());
     }
 
     @ExceptionHandler(ValueExceededException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValueExceeded(ValueExceededException e) {
-        log.error("Value Exceeded");
+        log.error("Value Exceeded: {}", e.getMessage(), e);
         return new ErrorResponse("Value Exceeded", e.getMessage());
     }
 }
