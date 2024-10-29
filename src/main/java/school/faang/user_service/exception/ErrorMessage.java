bb555b985ab8468f2424ff8bb72cb4b3820f0ea9
UrmatBolotbek <urmar9797@gmail.com>
2024-10-29 15:01:57 +0100

    add DataValidationException and ErrorMessage

---
+++ src/main/java/school/faang/user_service/exception/ErrorMessage.java
@@ -0,0 +1,7 @@
+package school.faang.user_service.exception;
+
+public class ErrorMessage {
+    public static final String RECOMMENDATION_CONTENT = "THE TEXT OF THE RECOMMENDATION CANNOT BE EMPTY";
+    public static final String RECOMMENDATION_AUTHOR = "THE AUTHOR OF THE RECOMMENDATION IS NOT SPECIFIED";
+    public static final String RECOMMENDATION_RECEIVER = "THE RECEIVER OF THE RECOMMENDATION IS NOT SPECIFIED";
+}
