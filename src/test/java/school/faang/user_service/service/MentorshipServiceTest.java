4db38ce434908bf77a4ab987c36d21a8dbe10dbc
Roma Nifontov <romannif@mail.ru>
2024-10-29 14:45:30 +0500

    коммит2

--- src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -6,10 +6,15 @@ import org.mockito.Mock;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 public class MentorshipServiceTest {
+
+    private final long NON_EXIST_USER_ID = 0L;
 
     @InjectMocks
     private MentorshipService service;
@@ -18,15 +23,11 @@ public class MentorshipServiceTest {
     private UserRepository repository;
 
     @Test
-    public void testGetMenteesWithNoSuchElement() {
-//        assertEquals()
-
-    }
-
-    @Test
     public void testGetMenteesWithNoMenteesForMentor() {
-
+//        when(repository.findById(NON_EXIST_USER_ID)).thenReturn(Optional.ofNullable());
+        assertThrows(NoSuchMethodException.class, () -> service.getMentees(NON_EXIST_USER_ID));
     }
+
 
     @Test
     public void testGetMenteesWithMenteesForMentor() {
