d3d95b3ff93e5d0763df5e0e689edb89af4208e4
Алексей Беляев <belyaevayur@intech.rshb.ru>
2024-10-30 16:49:36 +0300

    BJS2-40539 Участвовать в событии (без тестов)

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -1,7 +1,10 @@
 package school.faang.user_service.controller.event;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +17,7 @@ import school.faang.user_service.utilities.UrlUtils;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(UrlUtils.MAIN_URL + UrlUtils.V1 + UrlUtils.EVENTS)
@@ -21,24 +25,24 @@ public class EventParticipationController {
     private final EventParticipationService eventParticipationService;
 
     @PutMapping(UrlUtils.ID + UrlUtils.REGISTER)
-    public ResponseEntity<Void> registerParticipant(@PathVariable("id") Long eventId, @RequestParam("userId") Long userId) {
+    public ResponseEntity<Void> registerParticipant(@PathVariable("id") @Min(1) Long eventId, @RequestParam("userId") @Min(0) @Max(Long.MAX_VALUE) Long userId) {
         eventParticipationService.registerParticipant(eventId, userId);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping(UrlUtils.ID + UrlUtils.UNREGISTER)
-    public ResponseEntity<Void> unregisterParticipant(@PathVariable("id") Long eventId, @RequestParam("userId") Long userId) {
+    public ResponseEntity<Void> unregisterParticipant(@PathVariable("id") @Min(1) Long eventId, @RequestParam("userId") @Min(0) @Max(Long.MAX_VALUE) Long userId) {
         eventParticipationService.unregisterParticipant(eventId, userId);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping(UrlUtils.ID + UrlUtils.PARTICIPANTS)
-    public ResponseEntity<List<UserDto>> getParticipant(@PathVariable("id") Long eventId) {
+    public ResponseEntity<List<UserDto>> getParticipant(@PathVariable("id") @Min(1) Long eventId) {
         return ResponseEntity.ok(eventParticipationService.getParticipant(eventId));
     }
 
     @GetMapping(UrlUtils.ID + UrlUtils.PARTICIPANTS + UrlUtils.AMOUNT)
-    public ResponseEntity<Long> getParticipantsCount(@PathVariable("id") Long eventId) {
+    public ResponseEntity<Long> getParticipantsCount(@PathVariable("id") @Min(1) Long eventId) {
         return ResponseEntity.ok(eventParticipationService.getParticipantsCount(eventId));
     }
 }
--- src/main/java/school/faang/user_service/handler/ExceptionApiHandler.java
+++ src/main/java/school/faang/user_service/handler/ExceptionApiHandler.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.handler;
 
+import jakarta.validation.ValidationException;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionApiHandler {
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorMessage> notFoundException(IllegalArgumentException exception) {
+    public ResponseEntity<ErrorMessage> illegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorMessage(exception.getMessage()));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorMessage> validationException(ValidationException exception) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorMessage(exception.getMessage()));
