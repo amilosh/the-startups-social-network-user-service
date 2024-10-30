d0d966b3de20a22ff9f1785066dee8886794cf97
daurenberdibekov <berder08@gmail.com>
2024-10-30 17:01:39 +0100

    Рефакторинг: добавил валидацию полей к ДТО

--- src/main/java/school/faang/user_service/dto/UserDTO.java
+++ src/main/java/school/faang/user_service/dto/UserDTO.java
@@ -1,8 +1,15 @@
 package school.faang.user_service.dto;
 
+import jakarta.validation.constraints.NotNull;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+
 public record UserDTO(
-        long id,
+        @NotNull(groups = After.class)
+        Long id,
+        @NotNull(groups = Before.class)
         String username,
+        @NotNull(groups = Before.class)
         String email
 ) {
 }
