9fc57cc7af5bd13af66491551e34efaa6e0bd053
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-01 00:02:12 +1000

    new function for mapping list of entities

--- src/main/java/school/faang/user_service/mapper/mentorship/MentorshipRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/mentorship/MentorshipRequestMapper.java
@@ -5,9 +5,13 @@ import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.MentorshipRequestDto;
 import school.faang.user_service.entity.MentorshipRequest;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MentorshipRequestMapper {
     MentorshipRequest toEntity(MentorshipRequestDto requestDto);
 
     MentorshipRequestDto toDto(MentorshipRequest request);
+
+    List<MentorshipRequestDto> toDtoList(List<MentorshipRequest> requests);
 }
