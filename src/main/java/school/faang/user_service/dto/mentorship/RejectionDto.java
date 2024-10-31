519618974b7cb7f1f561721790e43430906338f6
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-01 02:11:59 +1000

    created rejectionDto

--- src/main/java/school/faang/user_service/dto/mentorship/RejectionDto.java
+++ src/main/java/school/faang/user_service/dto/mentorship/RejectionDto.java
@@ -1,4 +1,14 @@
 package school.faang.user_service.dto.mentorship;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class RejectionDto {
+    @NotBlank
+    private String reason;
 }
