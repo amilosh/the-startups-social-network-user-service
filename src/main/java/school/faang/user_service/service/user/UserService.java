cf373fb5ca5801a099afb64b5e11e19d4b77cf8b
“Andrey <aiegorovceo@yandex.ru>
2024-10-31 18:50:27 +0300

    feat: upgrade UserService

--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -1,12 +1,21 @@
 package school.faang.user_service.service.user;
 
+
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.repository.UserRepository;
 
 
-@Transactional
-public User getUserById(Long userId) {
-    return userRepository.findById(userId)
-            .orElseThrow(() -> new EntityNotFoundException("User with this id does not exist in the database"));
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User with this id does not exist in the database"));
+    }
 }
