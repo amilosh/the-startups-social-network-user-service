5c3ee289fc27e610351eb59ccc3f8fb949fe7f63
“Andrey <aiegorovceo@yandex.ru>
2024-10-31 19:37:47 +0300

    feat: Add SkillValidator to validate all skill IDs exist in the database

---
+++ src/main/java/school/faang/user_service/validator/skill/SkillValidator.java
@@ -0,0 +1,26 @@
+package school.faang.user_service.validator.skill;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.entity.Skill;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.repository.SkillRepository;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SkillValidator {
+    private final SkillRepository skillRepository;
+
+    public void validateAllSkillsIdsExist(List<Long> ids) {
+        if (ids != null && !ids.isEmpty()) {
+            List<Skill> skills = skillRepository.findAllById(ids);
+            if (skills.size() != ids.size()) {
+                throw new DataValidationException("There are no skills with those ids");
+            }
+        }
+    }
+}
