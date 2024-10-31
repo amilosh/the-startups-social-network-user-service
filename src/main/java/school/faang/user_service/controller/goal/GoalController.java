f72c393af77ec998cb37ca0be9a5da0dce60f917
“Andrey <aiegorovceo@yandex.ru>
2024-10-31 23:27:08 +0300

    feat: add logging to GoalController methods using SLF4J

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -3,15 +3,16 @@ package school.faang.user_service.controller.goal;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.GoalFilterDto;
-import school.faang.user_service.entity.goal.GoalStatus;
 import school.faang.user_service.service.goal.GoalService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/goals")
 @AllArgsConstructor
@@ -23,6 +24,7 @@ public class GoalController {
     @ResponseStatus(HttpStatus.CREATED)
     public GoalDto createGoal(@PathVariable("userId") @Positive Long userId,
                               @RequestBody @Valid GoalDto goalDto) {
+        log.info("Creating goal for user with ID: {}", userId);
         return goalService.createGoal(userId, goalDto);
     }
 
@@ -30,12 +32,14 @@ public class GoalController {
     @ResponseStatus(HttpStatus.OK)
     public GoalDto updateGoal(@PathVariable("goalId") @Positive Long goalId,
                               @RequestBody @Valid GoalDto goal) {
+        log.info("Updating goal with ID: {}", goalId);
         return goalService.updateGoal(goalId, goal);
     }
 
     @DeleteMapping("/{goalId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteGoal(@PathVariable("goalId") @Positive Long goalId) {
+        log.info("Deleting goal with ID: {}", goalId);
         goalService.deleteGoal(goalId);
     }
 
@@ -43,13 +47,15 @@ public class GoalController {
     @ResponseStatus(HttpStatus.OK)
     public List<GoalDto> findSubtasksByGoalId(@PathVariable("goalId") @Positive Long goalId,
                                               GoalFilterDto filterDto) {
+        log.info("Finding subtasks for goal ID: {} with filter: {}", goalId, filterDto);
         return goalService.findSubtasksByGoalId(goalId, filterDto);
     }
 
     @GetMapping("/users/{userId}")
     @ResponseStatus(HttpStatus.OK)
     public List<GoalDto> findGoalsByUserId(@PathVariable("userId") @Positive Long userId,
-                                        GoalFilterDto filterDto) {
+                                           GoalFilterDto filterDto) {
+        log.info("Finding goals for user ID: {} with filter: {}", userId, filterDto);
         return goalService.findGoalsByUserId(userId, filterDto);
     }
 }
