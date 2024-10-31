3c7389d1ff758afd3f09e91b6c4e7ac197763409
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-31 23:07:03 +1000

    basic filter (extend RequestFilter) added

---
+++ src/main/java/school/faang/user_service/service/mentorship/filter/RequestDescriptionFilter.java
@@ -0,0 +1,20 @@
+package school.faang.user_service.service.mentorship.filter;
+
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.RequestFilterDto;
+import school.faang.user_service.entity.MentorshipRequest;
+
+import java.util.stream.Stream;
+
+@Component
+public class RequestDescriptionFilter implements RequestFilter {
+    @Override
+    public boolean isApplicable(RequestFilterDto filterDto) {
+        return filterDto.getDescriptionPattern() != null && !filterDto.getDescriptionPattern().isBlank();
+    }
+
+    @Override
+    public Stream<MentorshipRequest> apply(Stream<MentorshipRequest> requests, RequestFilterDto filterDto) {
+        return requests.filter(request -> request.getDescription().contains(filterDto.getDescriptionPattern()));
+    }
+}
---
+++ src/main/java/school/faang/user_service/service/mentorship/filter/RequestStatusFilter.java
@@ -0,0 +1,22 @@
+package school.faang.user_service.service.mentorship.filter;
+
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.RequestFilterDto;
+import school.faang.user_service.entity.MentorshipRequest;
+
+import java.util.stream.Stream;
+
+@Component
+public class RequestStatusFilter implements RequestFilter {
+    @Override
+    public boolean isApplicable(RequestFilterDto filterDto) {
+        return filterDto.getStatus() != null;
+    }
+
+    @Override
+    public Stream<MentorshipRequest> apply(Stream<MentorshipRequest> requests, RequestFilterDto filterDto) {
+        return requests.filter(request -> request.getStatus() == filterDto.getStatus());
+    }
+}
+
+
