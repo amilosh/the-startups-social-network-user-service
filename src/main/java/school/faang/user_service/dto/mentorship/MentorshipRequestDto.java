01bfbee156990def460ec51a7e36c0fd4556953c
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-01 14:38:20 +1000

    dto is only for the return value

--- src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestDto.java
+++ src/main/java/school/faang/user_service/dto/mentorship/MentorshipRequestDto.java
@@ -1,35 +1,20 @@
 package school.faang.user_service.dto.mentorship;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import school.faang.user_service.entity.RequestStatus;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class MentorshipRequestDto {
     private Long id;
-
-    @NotBlank
-    @Size(min = 1, max = 4096)
     private String description;
-
-    @NotNull
     private Long requesterId;
-
-    @NotNull
     private Long receiverId;
-
-    @NotNull
     private RequestStatus status;
-
+    private String rejectionReason;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
 }
