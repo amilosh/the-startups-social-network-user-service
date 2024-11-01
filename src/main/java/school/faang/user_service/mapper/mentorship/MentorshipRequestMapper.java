014beecbe24515aabb47e3364846f9f20766741e
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-01 14:45:55 +1000

    MentorshipRequestMapper fixed

--- src/main/java/school/faang/user_service/mapper/mentorship/MentorshipRequestMapper.java
+++ src/main/java/school/faang/user_service/mapper/mentorship/MentorshipRequestMapper.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.mapper.mentorship;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import school.faang.user_service.dto.mentorship.MentorshipRequestCreationDto;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
 import school.faang.user_service.entity.MentorshipRequest;
 
@@ -9,9 +11,13 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MentorshipRequestMapper {
-    MentorshipRequest toEntity(MentorshipRequestDto requestDto);
+    @Mapping(source = "requesterId", target = "requester.id")
+    @Mapping(source = "receiverId", target = "receiver.id")
+    MentorshipRequest toMentorshipRequest(MentorshipRequestCreationDto requestDto);
 
-    MentorshipRequestDto toDto(MentorshipRequest request);
+    @Mapping(source = "requester.id", target = "requesterId")
+    @Mapping(source = "receiver.id", target = "receiverId")
+    MentorshipRequestDto toMentorshipRequestDto(MentorshipRequest request);
 
     List<MentorshipRequestDto> toDtoList(List<MentorshipRequest> requests);
 }
