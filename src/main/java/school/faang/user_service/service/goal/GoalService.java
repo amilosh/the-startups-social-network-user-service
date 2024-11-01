b85a8561a0aaad3444a2c71bcb102fff7591c10c
“Andrey <aiegorovceo@yandex.ru>
2024-11-01 04:13:04 +0300

    refactor: upgrade createGoal method

--- src/main/java/school/faang/user_service/service/goal/GoalService.java
+++ src/main/java/school/faang/user_service/service/goal/GoalService.java
@@ -1,10 +1,10 @@
 package school.faang.user_service.service.goal;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.GoalFilterDto;
 import school.faang.user_service.entity.Skill;
@@ -14,6 +14,7 @@ import school.faang.user_service.entity.goal.GoalStatus;
 import school.faang.user_service.filter.goal.GoalFilter;
 import school.faang.user_service.mapper.goal.GoalMapper;
 import school.faang.user_service.repository.SkillRepository;
+import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.service.user.UserService;
 import school.faang.user_service.validator.goal.GoalValidator;
@@ -34,19 +35,50 @@ public class GoalService {
     private final SkillRepository skillRepository;
     private final UserService userService;
     private final SkillValidator skillValidator;
+    private final UserRepository userRepository;
 
     @Transactional
     public GoalDto createGoal(Long userId, GoalDto goalDto) {
-        User user = userService.getUserById(userId);
-        goalValidator.validateUserGoalLimit(userId);
-
-        Goal goal = createGoalEntity(goalDto, user);
-
-        return goalMapper.toGoalDto(goal);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found by Id: " + userId));
+        Goal goalToSave = goalMapper.toGoal(goalDto);
+        if (goalDto.getParentId() != null) {
+            Goal parentGoal = goalRepository.findById(goalDto.getParentId())
+                    .orElseThrow(() -> new EntityNotFoundException("Goal not found by Id: " + goalDto.getParentId()));
+            goalToSave.setParent(parentGoal);
+        }
+        //todo: Добавить проверку скилл айди
+        if (goalDto.getSkillIds() != null) {
+            Goal parentGoal = goalRepository.findById(goalDto.getParentId())
+                    .orElseThrow(() -> new EntityNotFoundException("Goal not found by Id: " + goalDto.getParentId()));
+            goalToSave.setParent(parentGoal);
+        }
+        if (goalDto.getMentorId() != null) {
+            User mentor = userRepository.findById(goalDto.getMentorId())
+                    .orElseThrow(() -> new EntityNotFoundException("User not found by Id: " + goalDto.getMentorId()));
+            if (!user.getMentors().contains(mentor)) {
+            //todo: Добавить кастомную ошибку
+                throw new EntityNotFoundException();
+            }
+            goalToSave.setMentor(mentor);
+        }
+        if (goalDto.getDeadline() != null) {
+            goalToSave.setDeadline(goalDto.getDeadline());
+        }
+        goalToSave.setStatus(GoalStatus.ACTIVE);
+        Goal savedGoal = goalRepository.save(goalToSave);
+        return goalMapper.toGoalDto(savedGoal);
     }
 
     @Transactional
     public GoalDto updateGoal(long goalId, GoalDto goalDto) {
+
+        Goal goalToUpdate = goalRepository.findById(goalId)
+                .orElseThrow(() -> new EntityNotFoundException("Goal not found by Id: " + goalId));
+        if(goalDto.getParentId() != null) {}
+
+
+
         Goal existingGoal = getGoalById(goalId);
 
         Goal updatedGoal = updateGoalEntity(existingGoal, goalDto);
