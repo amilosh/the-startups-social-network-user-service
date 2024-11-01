dc87108243e13c0930b5f7f91bc47a70b5ce1c1c
Matvey <mashptz@gmail.com>
2024-11-01 18:29:24 +0300

    Draft

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 import school.faang.user_service.dto.recommendation.RecommendationDto;
+import school.faang.user_service.dto.recommendation.SkillOfferDto;
 import school.faang.user_service.service.recommendation.RecommendationService;
 import school.faang.user_service.validator.recommendation.ControllerRecommendationValidator;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -15,12 +18,23 @@ public class RecommendationController {
     private final RecommendationService recommendationService;
     private final ControllerRecommendationValidator recommendationValidator;
 
-    public void updateRecommendation(RecommendationDto updated) {
+    public void updateRecommendation(RecommendationDto upRecommendation) {
+        log.info("A request has been received to update the recommendation {}", upRecommendation);
+        recommendationValidator.validateContentRecommendation(upRecommendation.getContent());
     }
 
-    public Long giveRecommendation(RecommendationDto recommendation) {
-        log.info("The request {} for a recommendation has been received", recommendation);
+    public RecommendationDto giveRecommendation(RecommendationDto recommendation) {
+        log.info("A request has been received for a recommendation {}", recommendation);
         recommendationValidator.validateContentRecommendation(recommendation.getContent());
         return recommendationService.giveRecommendation(recommendation);
+    }
+
+    public void deleteRecommendation(RecommendationDto delRecommendation) {
+        log.info("A request was received to delete the recommendation {}", delRecommendation);
+    }
+
+    public List<SkillOfferDto> getAllUserRecommendations(long receiverId) {
+
+        return null;
     }
 }
