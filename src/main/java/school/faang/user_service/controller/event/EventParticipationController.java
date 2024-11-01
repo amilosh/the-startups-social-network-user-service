4a6cfec0fa1de91c53269080d281424571c8fb85
lomuserg <sergeymu2015@yandex.ru>
2024-11-01 16:51:51 +0300

    BJS2-40531 unregisterParticipant(), getParticipant(), getParticipantsCount(). UserMapper

--- src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
+++ src/main/java/school/faang/user_service/controller/event/EventParticipationController.java
@@ -1,12 +1,13 @@
 package school.faang.user_service.controller.event;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.service.event.EventParticipationService;
 
 @Component
 public class EventParticipationController {
-    private EventParticipationService eventParticipationService;
+    private final EventParticipationService eventParticipationService;
 
     @Autowired
     EventParticipationController(EventParticipationService eventParticipationService){
--- src/main/java/school/faang/user_service/service/event/EventParticipationService.java
+++ src/main/java/school/faang/user_service/service/event/EventParticipationService.java
@@ -4,30 +4,51 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.event.EventParticipationRepository;
+
+import java.util.List;
 
 @Component
 @Slf4j
 public class EventParticipationService {
-    private EventParticipationRepository eventParticipationRepository;
+    private final EventParticipationRepository eventParticipationRepository;
 
     @Autowired
     public EventParticipationService(EventParticipationRepository eventParticipationRepository) {
         this.eventParticipationRepository = eventParticipationRepository;
     }
 
-    public void registerParticipant(long eventId, long userId) {
-        boolean userExists = eventParticipationRepository
+    private boolean userExists(long eventId, long userId){
+        return eventParticipationRepository
                 .findAllParticipantsByEventId(eventId)
                 .stream()
                 .anyMatch(user -> user.getId() == userId);
+    }
 
-        if (userExists) {
-            log.info("Пользователь зарегистрирован");
+    public void registerParticipant(long eventId, long userId) {
+        if (!userExists(eventId, userId)) {
             eventParticipationRepository.register(eventId, userId);
+            log.info("User registered");
         } else {
-            log.info("Пользователь существует");
+            log.info("User exists");
         }
     }
 
+    public void unregisterParticipant(long eventId, long userId){
+        if (userExists(eventId, userId)) {
+            eventParticipationRepository.unregister(eventId, userId);
+            log.info("User unregistered");
+        } else {
+            log.info("User not found");
+        }
+    }
+
+    public List<User> getParticipant(long eventId){
+        return eventParticipationRepository.findAllParticipantsByEventId(eventId);
+    }
+
+    public int getParticipantsCount(long eventId){
+        return eventParticipationRepository.countParticipants(eventId);
+    }
 }
