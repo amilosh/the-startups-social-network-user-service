aa914ec71f1e7c4c9d4960454afbd7e8f1dc7da2
Naku <polevaya.nastena@internet.ru>
2024-11-02 16:16:10 +0300

    организация событий идеи двух метод

--- src/main/java/school/faang/user_service/service/EventService.java
+++ src/main/java/school/faang/user_service/service/EventService.java
@@ -12,6 +12,7 @@ import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.filters.event.EventFilter;
 import school.faang.user_service.mapper.EventMapper;
+import school.faang.user_service.mapper.SkillMapper;
 import school.faang.user_service.repository.event.EventRepository;
 
 import java.util.List;
@@ -24,7 +25,10 @@ public class EventService {
     private final EventRepository eventRepository;
     private final UserService userService;
     private final EventMapper eventMapper;
+    private final SkillMapper skillMapper;
     private final List<EventFilter> eventFilters;
+
+
 
     public EventDto create(EventDto eventDto) {
         log.info("Processing creation of event with title: {}", eventDto.title());
@@ -36,7 +40,12 @@ public class EventService {
         Event event = eventMapper.toEntity(eventDto);
         event.setOwner(userOwner);
 
+        if (eventExists(event)) {
+            throw new DataValidationException("You do not create event, because this event already create");
+        }
+
         Event savedEvent = eventRepository.save(event);
+
         log.info("Event saved with ID: {}", savedEvent.getId());
 
         return eventMapper.toDto(event);
@@ -49,7 +58,10 @@ public class EventService {
     }
 
     public List<EventDto> getEventsByFilter(EventFilterDto filters) {
+        log.info("Fetching events with filters: {}", filters);
+
         Stream<Event> allEvents = eventRepository.findAll().stream();
+
         eventFilters.stream()
                 .filter(filter -> filter.isApplicable(filters))
                 .forEach(filter -> filter.apply(allEvents, filters));
@@ -57,9 +69,39 @@ public class EventService {
         return eventMapper.toDtoList(allEvents.toList());
     }
 
+    public void deleteEvent(Long eventId) {
+        if (!eventRepository.existsById(eventId)) {
+            throw new DataValidationException("Event not found");
+        }
+        eventRepository.deleteById(eventId);
+        log.info("Event with ID: {} deleted", eventId);
+    }
+
+    public int updateEvent(EventDto eventDto) {
+        User userOwner = userService.getUserById(eventDto.ownerId());
+
+        log.debug("Validating user skills for owner: {}", userOwner);
+        validateUserSkillsForEvent(userOwner, eventDto);
+
+        Event event = eventMapper.toEntity(eventDto);
+
+        eventRepository.save(event);
+
+        return event.getAttendees().size();
+    }
+
+    public long  getAttendeesCount(Long eventId){
+
+        return eventRepository.count(getEventById(eventId));
+    }
+
+    public Event getEventById(Long eventId){
+        return   eventRepository.findById(eventId).orElseThrow(() -> new DataValidationException("Event do not found"));
+    }
+
     private void validateUserSkillsForEvent(User userOwner, EventDto eventDto) {
         List<SkillDto> relatedSkills = eventDto.relatedSkills();
-        List<Skill> userOwnerSkills = userOwner.getSkills();
+        List<SkillDto> userOwnerSkills = skillMapper.toDtoList(userOwner.getSkills());
 
         log.debug("Checking if user has required skills: {}", relatedSkills);
 
@@ -71,11 +113,21 @@ public class EventService {
         log.info("User {} has all required skills to create event", userOwner.getId());
     }
 
-    private boolean userHasSkills(List<Skill> userOwnerSkills, List<SkillDto> relatedSkills) {
+    private boolean userHasSkills(List<SkillDto> userOwnerSkills, List<SkillDto> relatedSkills) {
         return relatedSkills.stream()
                 .allMatch(relatedSkill -> userOwnerSkills.stream()
-                        .anyMatch(userSkill -> userSkill.getTitle().equals(relatedSkill.title()))
+                        .anyMatch(userSkill -> userSkill.equals(relatedSkill))
                 );
     }
-}
 
+    private boolean eventExists(Event event) {
+        return eventRepository.existsByTitleAndStartDateAndEndDateAndOwnerAndLocationAndMaxAttendees(
+                event.getTitle(),
+                event.getStartDate(),
+                event.getEndDate(),
+                event.getOwner(),
+                event.getLocation(),
+                event.getMaxAttendees()
+        );
+    }
+}
