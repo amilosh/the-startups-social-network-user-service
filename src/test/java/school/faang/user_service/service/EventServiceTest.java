8a2b1ced4af53ea03e067f0d7011ffba0989c1b9
Naku <polevaya.nastena@internet.ru>
2024-11-02 23:04:44 +0300

    организация событий emptytest

---
+++ src/test/java/school/faang/user_service/service/EventServiceTest.java
@@ -0,0 +1,36 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getEvent() {
+    }
+
+    @Test
+    void getEventsByFilter() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void getOwnedEvents() {
+    }
+
+    @Test
+    void getParticipatedEvents() {
+    }
+}
