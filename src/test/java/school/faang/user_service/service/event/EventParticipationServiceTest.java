eaf718b3e01a365fbc3f5c2f2c437337081fa9f6
lomuserg <sergeymu2015@yandex.ru>
2024-11-02 14:22:16 +0300

    BJS2-40531 testRegisterParticipant

--- src/test/java/school/faang/user_service/service/event/EventParticipationServiceTest.java
+++ src/test/java/school/faang/user_service/service/event/EventParticipationServiceTest.java
@@ -1,12 +1,26 @@
 package school.faang.user_service.service.event;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.exceptions.UserAlreadyRegisteredException;
 import school.faang.user_service.repository.event.EventParticipationRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class EventParticipationServiceTest {
 
     @Mock
@@ -28,7 +42,39 @@ public class EventParticipationServiceTest {
     }
 
     @Test
-    public void testRegisterParticipant_UserRegistered(){
+    @DisplayName("Проверка \"Пользователь зарегистрировался\"")
+    public void testRegisterParticipant_UserRegistered() {
+        when(eventParticipationRepository.findAllParticipantsByEventId(eventId)).thenReturn(new ArrayList<>());
+
+        eventParticipationService.registerParticipant(eventId, userId);
+
+        verify(eventParticipationRepository, times(1)).register(eventId, userId);
+        verify(eventParticipationRepository, times(1)).findAllParticipantsByEventId(eventId);
+    }
+
+    @Test
+    @DisplayName("Проверка \"Пользователь уже зарегистрирован\"")
+    public void testRegisterParticipant_UserAlreadyRegistered() {
+        List<User> participants = List.of(testUser);
+        when(eventParticipationRepository.findAllParticipantsByEventId(eventId)).thenReturn(participants);
+
+        assertThrows(UserAlreadyRegisteredException.class, () -> {
+            eventParticipationService.registerParticipant(eventId, userId);
+        });
+
+        verify(eventParticipationRepository, never()).register(eventId, userId);
+        verify(eventParticipationRepository, times(1)).findAllParticipantsByEventId(eventId);
+    }
+
+    @Test
+    @DisplayName("Проверка \"Пользователь отменил регистрацию\"")
+    public void testUnregisterParticipant_UserUnregistered() {
+
+    }
+
+    @Test
+    @DisplayName("Проверка \"Пользователь не зарегистрирован\"")
+    public void testUnregisterParticipant_UserIsNotRegistered() {
 
     }
 }
