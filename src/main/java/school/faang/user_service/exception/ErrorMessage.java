23e3d15988425dba4ea613323c7e9fea594d8731
UrmatBolotbek <urmar9797@gmail.com>
2024-11-02 04:09:48 +0100

    добавил новое сообщение

--- src/main/java/school/faang/user_service/exception/ErrorMessage.java
+++ src/main/java/school/faang/user_service/exception/ErrorMessage.java
@@ -6,4 +6,5 @@ public class ErrorMessage {
     public static final String RECOMMENDATION_RECEIVER = "THE RECEIVER OF THE RECOMMENDATION IS NOT SPECIFIED";
     public static final String RECOMMENDATION_TIME_LIMIT = "AUTHOR ID  \"%s\" DID RECOMMENDATION FOR USER ID = \"%s\" LESS THAN \"%d\" MONTHS AGO";
     public static final String SKILL_NOT_EXIST = "SKILL WITH NAME \"%s\" DOES NOT EXIST IN SYSTEM";
+    public static final String NO_SKILL_OFFERS = "NO SKILL OFFERS FOUND FOR RECOMMENDATION";
 }
