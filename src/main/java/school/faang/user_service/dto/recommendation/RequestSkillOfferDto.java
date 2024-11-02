7cfeb045ac0ba98baa7c9cd9f5da075c234ba1fe
UrmatBolotbek <urmar9797@gmail.com>
2024-11-02 04:11:58 +0100

    добавил аннотацию @Builder

--- src/main/java/school/faang/user_service/dto/recommendation/RequestSkillOfferDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RequestSkillOfferDto.java
@@ -1,10 +1,13 @@
 package school.faang.user_service.dto.recommendation;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class RequestSkillOfferDto {
     private Long id;
     private Long skillId;
     private String skillTitle;
+    private Long recommendationId;
 }
