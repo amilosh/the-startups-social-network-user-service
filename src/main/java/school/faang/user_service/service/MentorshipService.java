e906bac1e8f5a0c7a3c46f56364802b51323588a
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-11-02 17:28:02 +0300

    BJS2-40483 - "Стать ментором".

---
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -0,0 +1,67 @@
+package school.faang.user_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.repository.mentorship.MentorshipRepository;
+import school.faang.user_service.exception.DataValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MentorshipService {
+
+    private final MentorshipRepository mentorshipRepository;
+    private final UserMapper userMapper;
+
+    public List<UserDto> getMentees(long userId) {
+        User user = getUser(userId);
+        UserDto userDto = userMapper.toDto(user);
+        if (userDto.getMenteeIds().isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<User> mentees = mentorshipRepository.findAllById(userDto.getMenteeIds());
+
+        return mentees.stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    public List<UserDto> getMentors(long userId) {
+        User user = getUser(userId);
+        UserDto userDto = userMapper.toDto(user);
+        if (userDto.getMentorIds().isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<User> mentors = mentorshipRepository.findAllById(userDto.getMentorIds());
+
+        return mentors.stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    public boolean deleteMentee(long menteeId, long mentorId) {
+        User mentor = getUser(mentorId);
+        List<User> mentees = mentor.getMentees();
+        return mentees.removeIf(mentee -> mentee.getId() == menteeId);
+    }
+
+    public boolean deleteMentor(long menteeId, long mentorId) {
+        User mentee = getUser(menteeId);
+        List<User> mentors = mentee.getMentors();
+        return mentors.removeIf(mentor -> mentor.getId() == mentorId);
+    }
+
+    private User getUser(long userId) {
+        if (userId < 0) {
+            throw new DataValidationException("User's id cannot be less than zero");
+        }
+
+        return mentorshipRepository.findById(userId)
+                .orElseThrow(() -> new DataValidationException("User not found"));
+    }
+}
---
+++ src/test/java/school/faang/user_service/service/MentorshipServiceTest.java
@@ -0,0 +1,268 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.dto.UserDto;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.mapper.UserMapperImpl;
+import school.faang.user_service.repository.mentorship.MentorshipRepository;
+import school.faang.user_service.exception.DataValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class MentorshipServiceTest {
+    @InjectMocks
+    private MentorshipService mentorshipService;
+
+    @Mock
+    private MentorshipRepository mentorshipRepository;
+
+    @Spy
+    private UserMapperImpl userMapper;
+
+    @Test
+    public void testGetMenteesWithNegativeId() {
+        User user = prepareData(-1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        String expectedMessage = "User's id cannot be less than zero";
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.getMentees(userDto.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testGetMenteesWithNonExistentId() {
+        User user = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = getUserDto(user);
+        String expectedMessage = "User not found";
+        when(mentorshipRepository.findById(userDto.getId()))
+                .thenReturn(Optional.empty());
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.getMentees(userDto.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    private UserDto getUserDto(User user) {
+        UserDto userDto = userMapper.toDto(user);
+        return userDto;
+    }
+
+    @Test
+    public void testGetMenteesWithEmptyList() {
+        User user = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        when(mentorshipRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+
+        List<UserDto> mentees = mentorshipService.getMentees(userDto.getId());
+
+        assertEquals(0, mentees.size());
+    }
+
+    @Test
+    public void testGetMenteesWithFilledList() {
+        User firstUser = prepareData(1, new ArrayList<>(), new ArrayList<>());
+        User secondUser = prepareData(2, new ArrayList<>(), new ArrayList<>());
+        User user = prepareData(1212, new ArrayList<>(List.of(firstUser, secondUser)), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        List<Long> expectedMentees = new ArrayList<>(List.of(1L, 2L));
+        when(mentorshipRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        when(mentorshipRepository.findAllById(userDto.getMenteeIds()))
+                .thenReturn(new ArrayList<>(List.of(firstUser, secondUser)));
+
+        List<UserDto> mentees = mentorshipService.getMentees(userDto.getId());
+
+        assertEquals(expectedMentees.size(), mentees.size());
+    }
+
+    @Test
+    public void testGetMentorsWithNegativeId() {
+        User user = prepareData(-1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        String expectedMessage = "User's id cannot be less than zero";
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.getMentors(userDto.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testGetMentorsWithNonExistentId() {
+        User user = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        String expectedMessage = "User not found";
+        when(mentorshipRepository.findById(userDto.getId()))
+                .thenReturn(Optional.empty());
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.getMentors(userDto.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testGetMentorsWithEmptyList() {
+        User user = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        UserDto userDto = userMapper.toDto(user);
+        when(mentorshipRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+
+        List<UserDto> mentors = mentorshipService.getMentors(userDto.getId());
+
+        assertEquals(0, mentors.size());
+    }
+
+    @Test
+    public void testGetMentorsWithFilledList() {
+        User firstUser = prepareData(1, new ArrayList<>(), new ArrayList<>());
+        User secondUser = prepareData(2, new ArrayList<>(), new ArrayList<>());
+        User user = prepareData(1212, new ArrayList<>(), new ArrayList<>(List.of(firstUser, secondUser)));
+        UserDto userDto = userMapper.toDto(user);
+        List<Long> expectedMentors = new ArrayList<>(List.of(1L, 2L));
+        when(mentorshipRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        when(mentorshipRepository.findAllById(userDto.getMentorIds()))
+                .thenReturn(new ArrayList<>(List.of(firstUser, secondUser)));
+
+        List<UserDto> mentors = mentorshipService.getMentors(userDto.getId());
+
+        assertEquals(expectedMentors.size(), mentors.size());
+    }
+
+    @Test
+    public void testDeleteMenteeWithNegativeMentorId() {
+        User mentor = prepareData(-1212, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        String expectedMessage = "User's id cannot be less than zero";
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.deleteMentee(mentee.getId(), mentor.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testDeleteMenteeWithNonExistentMentorId() {
+        User mentor = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(1313, new ArrayList<>(), new ArrayList<>());
+        String expectedMessage = "User not found";
+        when(mentorshipRepository.findById(mentor.getId()))
+                .thenReturn(Optional.empty());
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.deleteMentee(mentee.getId(), mentor.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testDeleteMenteeFromSingleValueList() {
+        User mentee = prepareData(1313, new ArrayList<>(), new ArrayList<>());
+        User mentor = prepareData(1212, new ArrayList<>(List.of(mentee)), new ArrayList<>());
+        when(mentorshipRepository.findById(mentor.getId()))
+                .thenReturn(Optional.of(mentor));
+
+        boolean resultAfterDelete = mentorshipService.deleteMentee(mentee.getId(), mentor.getId());
+        List<User> mentees = mentor.getMentees();
+
+        assertTrue(resultAfterDelete);
+        assertEquals(0, mentees.size());
+    }
+
+    @Test
+    public void testDeleteMenteeFromListWithMultipleValues() {
+        User firstMentee = prepareData(1313, new ArrayList<>(), new ArrayList<>());
+        User secondMentee = prepareData(1414, new ArrayList<>(), new ArrayList<>());
+        User mentor = prepareData(1212, new ArrayList<>(List.of(firstMentee, secondMentee)), new ArrayList<>());
+        when(mentorshipRepository.findById(mentor.getId()))
+                .thenReturn(Optional.of(mentor));
+
+        boolean resultAfterDelete = mentorshipService.deleteMentee(firstMentee.getId(), mentor.getId());
+        List<User> mentees = mentor.getMentees();
+
+        assertTrue(resultAfterDelete);
+        assertEquals(1, mentees.size());
+    }
+
+    @Test
+    public void testDeleteMentorWithNegativeMenteeId() {
+        User mentor = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(-1212, new ArrayList<>(), new ArrayList<>());
+        String expectedMessage = "User's id cannot be less than zero";
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.deleteMentor(mentee.getId(), mentor.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testDeleteMentorWithNonExistentMenteeId() {
+        User mentor = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(1313, new ArrayList<>(), new ArrayList<>());
+        String expectedMessage = "User not found";
+        when(mentorshipRepository.findById(mentee.getId()))
+                .thenReturn(Optional.empty());
+
+        DataValidationException exception = assertThrows(DataValidationException.class,
+                () -> mentorshipService.deleteMentor(mentee.getId(), mentor.getId()));
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void testDeleteMentorFromSingleValueList() {
+        User mentor = prepareData(1212, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(1313, new ArrayList<>(), new ArrayList<>(List.of(mentor)));
+        when(mentorshipRepository.findById(mentee.getId()))
+                .thenReturn(Optional.of(mentee));
+
+        boolean resultAfterDelete = mentorshipService.deleteMentor(mentee.getId(), mentor.getId());
+        List<User> mentors = mentee.getMentors();
+
+        assertTrue(resultAfterDelete);
+        assertEquals(0, mentors.size());
+    }
+
+    @Test
+    public void testDeleteMentorFromListWithMultipleValues() {
+        User firstMentor = prepareData(1313, new ArrayList<>(), new ArrayList<>());
+        User secondMentor = prepareData(1414, new ArrayList<>(), new ArrayList<>());
+        User mentee = prepareData(1212, new ArrayList<>(), new ArrayList<>(List.of(firstMentor, secondMentor)));
+        when(mentorshipRepository.findById(mentee.getId()))
+                .thenReturn(Optional.of(mentee));
+
+        boolean resultAfterDelete = mentorshipService.deleteMentor(mentee.getId(), secondMentor.getId());
+        List<User> mentors = mentee.getMentors();
+
+        assertTrue(resultAfterDelete);
+        assertEquals(1, mentors.size());
+    }
+
+    private User prepareData(long userId, List<User> mentees, List<User> mentors) {
+        User user = new User();
+        user.setId(userId);
+        user.setMentees(mentees);
+        user.setMentors(mentors);
+        return user;
+    }
+}
