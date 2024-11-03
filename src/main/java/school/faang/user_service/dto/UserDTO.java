fb0f0cdd5d5467a1443b6df23db4993ddb6fdce1
SerUKalinin <serukalinin@gmail.com>
2024-11-03 22:50:45 +0300

    Rewiew - убрать @Transactional - создать SubscriptionNotFoundException - создать SubscriptionAlreadyExistsException - изменить followUser - изменить unfollowUser -

--- src/main/java/school/faang/user_service/dto/UserDTO.java
+++ src/main/java/school/faang/user_service/dto/UserDTO.java
@@ -2,9 +2,11 @@ package school.faang.user_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private Long id;
     private String username;
