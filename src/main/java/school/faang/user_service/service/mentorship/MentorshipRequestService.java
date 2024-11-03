e752c545ba47b9863ff081ec05dd92625648ab3b
Roman Nifontov <romannif@mail.ru>
2024-11-03 21:35:12 +0500

    Добавил логи в задачу

--- src/main/java/school/faang/user_service/service/mentorship/MentorshipRequestService.java
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipRequestService.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.service.mentorship;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
 import school.faang.user_service.dto.mentorship.RejectionDto;
@@ -19,6 +20,7 @@ import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MentorshipRequestService {
 
     private final MentorshipRequestRepository mentorshipRequestRepository;
@@ -42,17 +44,22 @@ public class MentorshipRequestService {
         mentorshipRequest.setRequester(requester);
 
         receiver.getReceivedMentorshipRequests().add(mentorshipRequest);
+        log.info("Mentorship request is saved in the list of the recipient with ID {}", receiverId);
         requester.getSentMentorshipRequests().add(mentorshipRequest);
+        log.info("Mentorship request is saved in the list of the sender with ID {}", requesterId);
 
         mentorshipRequestRepository.save(mentorshipRequest);
         userRepository.save(requester);
+        log.info("Requester with ID {} is saved ib DB", requesterId);
         userRepository.save(receiver);
+        log.info("Receiver with ID {} is saved ib DB", receiverId);
+        log.info("Mentorship request with requestId {} saved", mentorshipRequest.getId());
     }
 
     public List<MentorshipRequestDto> getRequests(RequestFilterDto requestFilter) {
         Stream<MentorshipRequest> mentorshipRequests = mentorshipRequestRepository.findAll().stream();
         requestFilters.stream().filter(filter -> filter.isApplicable(requestFilter)).forEach(filter -> filter.apply(mentorshipRequests, requestFilter));
-
+        log.info("Getting a list of mentoring requests after filtering");
         return mentorshipRequestMapper.toMentorshipRequestDtoList(mentorshipRequests.toList());
     }
 
@@ -67,12 +74,18 @@ public class MentorshipRequestService {
             throw new IllegalArgumentException("Id " + requesterId + " is already on the mentis list " + receiverId);
         }
         receiver.getMentees().add(requester);
+        log.info("The requester {} was successfully added to the receivers {} list of mentees", requesterId, receiverId);
         requester.getMentors().add(receiver);
+        log.info("The receiver {} was successfully added to the requesters {} list of mentors", receiverId, requesterId);
         userRepository.save(requester);
+        log.info("The requester {} was successfully saved to DB", requesterId);
         userRepository.save(receiver);
+        log.info("The receiver {} was successfully saved to DB", receiverId);
+
 
         mentorshipRequest.setStatus(RequestStatus.ACCEPTED);
         mentorshipRequestRepository.save(mentorshipRequest);
+        log.info("Mentorship request with status ACCEPTED and requestId {} saved", mentorshipRequest.getId());
     }
 
     public void rejectRequest(long id, RejectionDto rejection) {
@@ -81,5 +94,7 @@ public class MentorshipRequestService {
         mentorshipRequest.setStatus(RequestStatus.REJECTED);
         mentorshipRequest.setRejectionReason(reason);
         mentorshipRequestRepository.save(mentorshipRequest);
+        log.info("Mentorship request with status REJECTED and requestId {} saved", mentorshipRequest.getId());
+
     }
 }
