c679f15ea00faf0e10956066686504ea3d9e33cf
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-11-03 21:45:35 +0500

    fix(BJS2-40530): event organization test
    -change some moments

--- src/main/java/school/faang/user_service/exception/ControllerMethodArgumentValidationExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/ControllerMethodArgumentValidationExceptionHandler.java
@@ -4,14 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerMethodArgumentValidationExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<DataValidationException>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
