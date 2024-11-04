e871535fe84c3bc6bbf0c9c2f2ab4b31cbcd88ff
Slava <leeteso97@gmail.com>
2024-11-04 11:29:56 +0500

    поправил конфликты при мердже

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -2,7 +2,8 @@ package school.faang.user_service.exception;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public class DataValidationException extends RuntimeException {
-    private String description;
+    public DataValidationException(String message) {
+        super(message);
+    }
 }
