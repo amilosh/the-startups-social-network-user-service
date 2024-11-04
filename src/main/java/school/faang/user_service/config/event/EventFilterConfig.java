a6f54f56e554a5ec60d610163e6a330c2a9c9346
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-11-04 09:34:40 +0500

    fix(BJS2-40530): event organization filter
    -change some moments in filters

--- src/main/java/school/faang/user_service/config/event/EventFilterConfig.java
+++ src/main/java/school/faang/user_service/config/event/EventFilterConfig.java
@@ -3,10 +3,10 @@ package school.faang.user_service.config.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import school.faang.user_service.service.event.EventFilter;
 import school.faang.user_service.service.event.EventService;
-import school.faang.user_service.service.event.EventTitleFilter;
-import school.faang.user_service.service.event.EventUserIdFilter;
+import school.faang.user_service.service.event.filter.EventFilter;
+import school.faang.user_service.service.event.filter.EventTitleFilter;
+import school.faang.user_service.service.event.filter.EventUserIdFilter;
 
 import java.util.ArrayList;
 import java.util.List;
