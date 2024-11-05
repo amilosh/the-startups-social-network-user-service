aa57520f254d5e42796b41ab23a2be7e252a18c3
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-11-05 20:50:37 +0500

    fix(BJS2-40530): request mappers
    added mappers to event controller endpoints

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -5,6 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
@@ -15,34 +21,42 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/v1/event")
 public class EventController {
     private final EventService eventService;
 
+    @PostMapping("create")
     public ResponseEntity<EventDto> create(@Valid EventDto eventDto) {
         return eventService.create(eventDto);
     }
 
-    public ResponseEntity<EventDto> getEvent(@NotNull Long eventId) {
+    @GetMapping("{eventId}")
+    public ResponseEntity<EventDto> getEvent(@NotNull @PathVariable Long eventId) {
         return eventService.getEvent(eventId);
     }
 
-    public ResponseEntity<EventDto> deleteEvent(@NotNull Long eventId) {
+    @DeleteMapping("{eventId}")
+    public ResponseEntity<EventDto> deleteEvent(@NotNull @PathVariable Long eventId) {
         return eventService.deleteEvent(eventId);
     }
 
+    @GetMapping("get-by-filters")
     public ResponseEntity<List<EventDto>> getEventsByFilter(EventFilterDto filter) {
         return eventService.getEventsByFilter(filter);
     }
 
+    @PutMapping
     public ResponseEntity<EventDto> updateEvent(@Valid EventDto event) {
         return eventService.updateEvent(event);
     }
 
-    public ResponseEntity<List<EventDto>> getOwnedEvents(@NotNull Long userId) {
+    @GetMapping("{userId}")
+    public ResponseEntity<List<EventDto>> getOwnedEvents(@NotNull @PathVariable Long userId) {
         return eventService.getOwnedEvents(userId);
     }
 
-    public ResponseEntity<List<EventDto>> getParticipatedEvents(long userId) {
+    @GetMapping("user-participated-events/{userId}")
+    public ResponseEntity<List<EventDto>> getParticipatedEvents(@NotNull @PathVariable Long userId) {
         return eventService.getParticipatedEvents(userId);
     }
 }
