74649bd2a51c74f8589e9a9da06f0663de467259
Aliyar <aliartalybov@gmail.com>
2024-11-05 16:56:54 +0300

    доработка комементариев

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import school.faang.user_service.entity.contact.PreferredContact;
 
 @Data
 @Builder
@@ -17,5 +18,10 @@ public class UserDto {
     private String phone;
     private String aboutMe;
     private String city;
+    private String country;
+    private String password;
+    private String telegramChatId;
     private Integer experience;
+    private PreferredContact preference;
+
 }
