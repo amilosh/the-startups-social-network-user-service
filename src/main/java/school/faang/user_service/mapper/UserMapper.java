eaaaf23521e9ad34bb55f526003727159f5873a8
Aliyar <aliartalybov@gmail.com>
2024-11-05 15:28:10 +0300

    доработка комементариев

--- src/main/java/school/faang/user_service/mapper/UserMapper.java
+++ src/main/java/school/faang/user_service/mapper/UserMapper.java
@@ -1,34 +1,30 @@
 package school.faang.user_service.mapper;
 
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-import school.faang.user_service.dto.user.UserDto;
+
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.entity.contact.Contact;
-import school.faang.user_service.entity.contact.ContactType;
 
-import java.util.List;
-
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
-    User toEntity(UserDto userDto);
-
-    @Mapping(target = "preference", source = "contactPreference.preference")
-    @Mapping(target = "telegramChatId", source = "contacts", qualifiedByName = "getTelegramContact")
+    @Mapping(target = "id", source = "user.id")
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "phone", source = "user.phone")
+    @Mapping(target = "aboutMe", source = "user.aboutMe")
+    @Mapping(target = "city", source = "user.city")
+    @Mapping(target = "experience", source = "user.experience")
     UserDto toDto(User user);
 
-    @Named("getTelegramContact")
-    default String getTelegramContact(List<Contact> contacts) {
-        if (contacts == null) {
-            return null;
-        }
-        return contacts.stream()
-                .filter(contact -> contact.getType() == ContactType.TELEGRAM)
-                .map(Contact::getContact)
-                .findFirst().orElse(null);
-    }
+    @Mapping(target = "id", source = "userDto.id")
+    @Mapping(target = "username", source = "userDto.username")
+    @Mapping(target = "email", source = "userDto.email")
+    @Mapping(target = "phone", source = "userDto.phone")
+    @Mapping(target = "aboutMe", source = "userDto.aboutMe")
+    @Mapping(target = "city", source = "userDto.city")
+    @Mapping(target = "experience", source = "userDto.experience")
+    User toEntity(UserDto userDto);
 }
