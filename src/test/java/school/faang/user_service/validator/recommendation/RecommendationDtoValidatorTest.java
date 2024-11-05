f7979ddab2211d1c56d6c43d20ecfe83db84a910
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-05 22:08:43 +0300

    commit: DataValidationException remove

--- src/test/java/school/faang/user_service/validator/recommendation/RecommendationDtoValidatorTest.java
+++ src/test/java/school/faang/user_service/validator/recommendation/RecommendationDtoValidatorTest.java
@@ -11,7 +11,6 @@ import school.faang.user_service.dto.recommendation.RequestRecommendationDto;
 import school.faang.user_service.dto.recommendation.SkillOfferDto;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.recommendation.Recommendation;
-import school.faang.user_service.exception.recommendation.DataValidationException;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRepository;
