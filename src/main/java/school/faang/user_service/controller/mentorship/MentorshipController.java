592b47cc7165501fe20ec1fd2a159b232cba6320
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-11-05 19:00:04 +0300

    Added @DeleteMapping and GetMapping

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -2,6 +2,9 @@ package school.faang.user_service.controller.mentorship;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.service.MentorshipService;
 
@@ -13,19 +16,23 @@ import java.util.List;
 public class MentorshipController {
     private final MentorshipService mentorshipService;
 
+    @GetMapping
     public List<UserDto> getMentees(long userId) {
         return mentorshipService.getMentees(userId);
     }
 
+    @GetMapping
     public List<UserDto> getMentors(long userId) {
         return mentorshipService.getMentors(userId);
     }
 
-    public void deleteMentee(long menteeId, long mentorId) {
+    @DeleteMapping("/{Id}/mentees/{menteeId}")
+    public void deleteMentee(@PathVariable long menteeId, @PathVariable("Id") long mentorId) {
         mentorshipService.deleteMentee(menteeId, mentorId);
     }
 
-    public void deleteMentor(long menteeId, long mentorId) {
+    @DeleteMapping("/{Id}/mentors/{mentorId}")
+    public void deleteMentor(@PathVariable("Id") long menteeId, @PathVariable long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
     }
 }
