c96da95b640b9a8f1ae1fa516f71133e7b36c72a
SerUKalinin <serukalinin@gmail.com>
2024-11-05 18:07:57 +0300

    в ParticipantRegistrationException и UnfollowException добавил @ResponseStatus(HttpStatus.BAD_REQUEST)

--- src/main/java/school/faang/user_service/exceptions/subscribe/UnfollowException.java
+++ src/main/java/school/faang/user_service/exceptions/subscribe/UnfollowException.java
@@ -3,7 +3,7 @@ package school.faang.user_service.exceptions.subscribe;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UnfollowException extends RuntimeException {
     public UnfollowException(String message, Throwable cause) {
         super(message, cause);
