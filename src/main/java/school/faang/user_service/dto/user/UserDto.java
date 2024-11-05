d637952da4b212de46826a3f75c4987cc61ec490
Aliyar <aliartalybov@gmail.com>
2024-11-05 15:24:10 +0300

    доработка комементариев

--- src/main/java/school/faang/user_service/dto/user/UserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserDto.java
@@ -11,4 +11,8 @@ public class UserDto {
     private Long id;
     private String username;
     private String email;
+    private String phone;
+    private String aboutMe;
+    private String city;
+    private String experience;
 }
