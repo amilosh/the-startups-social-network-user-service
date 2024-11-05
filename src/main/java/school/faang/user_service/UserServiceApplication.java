aeecf90268c3f271dc2c7cf6514a2bf82fc29768
Roman Nifontov <romannif@mail.ru>
2024-11-05 23:59:42 +0500

    промежуточный коммит

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients("school.faang.user_service.client")
 @OpenAPIDefinition(
         info = @Info(
-                title = "User Service",
+                title = "User Service Test",
                 version = "1.0.0")
 )
 public class UserServiceApplication {
--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -1,18 +1,30 @@
 package school.faang.user_service.controller.mentorship;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.service.mentorship.MentorshipService;
 
 import java.util.List;
 
-@Controller
+@RequestMapping("/mentorship")
+@RestController
 @RequiredArgsConstructor
 public class MentorshipController {
 
     private final MentorshipService mentorshipService;
 
+    @Operation (
+            summary = "получение всех ментис",
+            description = "бла бла бла"
+    )
+    @GetMapping("/mentees/{mentorId}")
     public List<UserDto> getMentees(long mentorId) {
         return mentorshipService.getMentees(mentorId);
     }
