bb19f39f2e8123fcad0dda00b7f3e75ed29da559
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-06 20:27:36 +1000

    fixed import

--- src/main/java/school/faang/user_service/service/mentorship/MentorshipRequestService.java
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipRequestService.java
@@ -1,6 +1,6 @@
 package school.faang.user_service.service.mentorship;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
