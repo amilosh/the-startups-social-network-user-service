3d28a186014cba3dc10102a20fd148b0ee568f86
Andrew Balashov <balashov83@gmail.com>
2024-11-06 16:30:19 +0300

    BJS2-40457 Fix: review fixes
    Exceptions: Exception for validation purposes was removed.
    Other: minor code cleaning works

--- src/main/java/school/faang/user_service/exception/skill/SkillDuplicateException.java
+++ src/main/java/school/faang/user_service/exception/skill/SkillDuplicateException.java
@@ -1,8 +1,5 @@
 package school.faang.user_service.exception.skill;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class SkillDuplicateException extends RuntimeException{
 
     public SkillDuplicateException(String message) {
