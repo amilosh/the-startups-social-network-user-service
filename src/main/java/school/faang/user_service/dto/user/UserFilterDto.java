9436fbd93583e4105eb9ff23f65fa610d48a61d3
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-08 00:52:20 +0300

    commit2: Search priority for premium users

--- src/main/java/school/faang/user_service/dto/user/UserFilterDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserFilterDto.java
@@ -3,15 +3,20 @@ package school.faang.user_service.dto.user;
 import lombok.Data;
 import school.faang.user_service.entity.premium.Premium;
 
-import java.time.LocalDateTime;
-
 @Data
 public class UserFilterDto {
+    private String namePattern;
+    private String phonePattern;
+    private String emailPattern;
+    private String aboutPattern;
+    private String contactPattern;
+    private String countryPattern;
+    private String cityPattern;
+    private String skillPattern;
+    private int experienceMin;
+    private int experienceMax;
+    private int page;
+    private int pageSize;
     private Long id;
     private Premium premium;
-    private String namePattern;
-    private String phone;
-    private LocalDateTime createdAfter;
-    private LocalDateTime createdBefore;
-    private Integer experience;
 }
