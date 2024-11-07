a4d251fc1ab9fce29cb8ccf33ab3cd5f1fed2a62
Roman <roma.koshetov@mail.ru>
2024-11-07 16:30:03 +0300

    clean some redundant imports

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -1,9 +1,5 @@
 package school.faang.user_service.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DataValidationException extends RuntimeException {
 
     public DataValidationException(String message) {
