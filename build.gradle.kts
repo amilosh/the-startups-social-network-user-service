26197b080975225eff5aac055759ada9a07e825d
Vakarash <max.vackarash@gmail.com>
2024-11-07 21:11:15 +0300

    Перенос dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.liquibase:liquibase-core")
     implementation("redis.clients:jedis:4.3.2")
     runtimeOnly("org.postgresql:postgresql")
+    testImplementation("com.h2database:h2")
 
     /**
      * Amazon S3
@@ -74,7 +75,6 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
-    testImplementation("com.h2database:h2")
 
     /**
      * Tests
