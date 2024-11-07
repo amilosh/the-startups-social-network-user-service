11522b88bf78975a56181cc9de360f3ceed416d4
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-11-07 17:19:47 +0300

    BJS2-40768 - refactor CI pipline

--- src/test/java/school/faang/user_service/service/SomeFailedTest.java
+++ src/test/java/school/faang/user_service/service/SomeFailedTest.java
@@ -2,14 +2,11 @@ package school.faang.user_service.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class SomeFailedTest {
     @Test
     public void testTrue() {
-        boolean result = true;
-        assertTrue(result);
+        assertTrue(false);
     }
 }
