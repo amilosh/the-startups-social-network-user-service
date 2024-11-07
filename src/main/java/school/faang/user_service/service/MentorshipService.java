f008f15615c53fdec3f7a069b5f373caf00f96c0
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-11-07 13:25:22 +0300

    Стать ментором
    Вынес проверки в отдельный метод checkParam
    
    BJS2-40463

--- src/main/java/school/faang/user_service/service/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -19,12 +19,18 @@ public class MentorshipService {
     private final MentorshipRepository mentorshipRepository;
     private final MentorshipMapper mentorshipMapper;
 
+    private void checkParam(long... ids) {
+        for (long id : ids) {
+            if (id <= 0) {
+                log.warn(id + "not found");
+                throw new IllegalArgumentException("Id not found");
+            }
+        }
+    }
+
     public List<UserDto> getMentees(long mentorId) {
         log.info("Fetching mentees for mentor with ID: {}", mentorId);
-        if (mentorId <= 0) {
-            log.warn("Invalid mentor ID: {}", mentorId);
-            return List.of();
-        }
+        checkParam(mentorId);
         return mentorshipRepository.findById(mentorId)
                 .map(mentor -> mentor.getMentees().stream()
                         .map(mentorshipMapper::toDto)
@@ -37,10 +43,7 @@ public class MentorshipService {
 
     public List<UserDto> getMentors(long menteeId) {
         log.info("Fetching mentors for mentee with ID: {}", menteeId);
-        if (menteeId <= 0) {
-            log.warn("Invalid mentee ID: {}", menteeId);
-            return List.of();
-        }
+        checkParam(menteeId);
         return mentorshipRepository.findById(menteeId)
                 .map(mentee -> mentee.getMentors().stream()
                         .map(mentorshipMapper::toDto)
@@ -53,20 +56,14 @@ public class MentorshipService {
 
     public void deleteMentee(long menteeId, long mentorId) {
         log.info("Deleting mentorship relationship for mentor ID: {} and mentee ID: {}", mentorId, menteeId);
-        if (menteeId <= 0 || mentorId <= 0) {
-            log.warn("Invalid mentee or mentor ID: menteeId={}, mentorId={}", menteeId, mentorId);
-            return;
-        }
+        checkParam(mentorId, menteeId);
         mentorshipRepository.deleteMentorship(menteeId, mentorId);
         log.info("Deleted mentorship relationship for mentor ID: {} and mentee ID: {}", mentorId, menteeId);
     }
 
     public void deleteMentor(long menteeId, long mentorId) {
         log.info("Deleting mentorship relationship for mentor ID: {} and mentee ID: {}", mentorId, menteeId);
-        if (menteeId <= 0 || mentorId <= 0) {
-            log.warn("Invalid mentee or mentor ID: menteeId={}, mentorId={}", menteeId, mentorId);
-            return;
-        }
+        checkParam(mentorId, menteeId);
         mentorshipRepository.deleteMentorship(menteeId, mentorId);
         log.info("Deleted mentorship relationship for mentor ID: {} and mentee ID: {}", mentorId, menteeId);
     }
