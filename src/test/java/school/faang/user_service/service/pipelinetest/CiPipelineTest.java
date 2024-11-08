6310ce4679ee25ccc6fb372f8017b07b5af5f34c
daurenberdibekov <berder08@gmail.com>
2024-11-08 15:49:53 +0100

    Add pipeline test

---
+++ src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
@@ -0,0 +1,13 @@
+package school.faang.user_service.service.pipelinetest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class CiPipelineTest {
+
+    @Test
+    void pipeline_shouldFail(){
+        fail();
+    }
+}
