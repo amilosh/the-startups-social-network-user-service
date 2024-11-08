5f1b59d1a980d6aea29ceb6630c0b80d5b7c2d3c
Geodakyan Yevgeniy <yevgengeodakyan@gmail.com>
2024-11-08 09:38:28 -0500

    SpringContext test

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,6 +74,9 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("com.h2database:h2")
+
 
     /**
      * Tests
---
+++ src/test/java/school/faang/user_service/ApplicationContextTest.java
@@ -0,0 +1,14 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ApplicationContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
