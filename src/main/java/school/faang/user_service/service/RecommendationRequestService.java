b4f8883e74af3f665fe5c69bf35e6c2d715be5db
gimadeev <6962009@gmail.com>
2024-11-09 12:27:51 +0300

    [BJS2-40439] Запрос рекомендации.

--- src/main/java/school/faang/user_service/service/RecommendationRequestService.java
+++ src/main/java/school/faang/user_service/service/RecommendationRequestService.java
@@ -1,42 +1,53 @@
 package school.faang.user_service.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.RecommendationRequestDto;
+import school.faang.user_service.dto.RejectionDto;
+import school.faang.user_service.dto.RequestFilterDto;
 import school.faang.user_service.entity.recommendation.RecommendationRequest;
+import school.faang.user_service.mapper.RecommendationRequestMapper;
 import school.faang.user_service.repository.SkillRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRepository;
 import school.faang.user_service.repository.recommendation.RecommendationRequestRepository;
 import school.faang.user_service.repository.recommendation.SkillRequestRepository;
+import school.faang.user_service.service.filters.recommendation.request.RecommendationRequestFilter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Stream;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class RecommendationRequestService {
     private final RecommendationRequestRepository recommendationRequestRepository;
     private final RecommendationRepository recommendationRepository;
     private final SkillRepository skillRepository;
     private final SkillRequestRepository skillRequestRepository;
+    private final RecommendationRequestMapper requestMapper;
+    private final List<RecommendationRequestFilter> recRequestFilters;
 
-    public Long create(RecommendationRequestDto recRequest) {
-        long createdRequestId = 0;
-        if (eligibleForRecommendation(recRequest)) {
-            createdRequestId = recommendationRepository.create(
-                    recRequest.getRequesterId(),
-                    recRequest.getReceiverId(),
-                    recRequest.getMessage()
+    public RecommendationRequestDto create(RecommendationRequestDto recRequestDto) {
+        RecommendationRequest request = requestMapper.toEntity(recRequestDto);
+
+        if (eligibleForRecommendation(recRequestDto)) {
+            long createdRequestId = recommendationRepository.create(
+                    request.getRequester().getId(),
+                    request.getReceiver().getId(),
+                    request.getMessage()
             );
-            recRequest.getSkills().forEach(skill ->
-                    skillRequestRepository.create(recRequest.getId(), skill.getId()));
+
+            request.setId(createdRequestId);
+            recRequestDto.getSkillsId().forEach(skillId -> skillRequestRepository.create(recRequestDto.getId(), skillId));
         }
-        return createdRequestId;
+        return requestMapper.toDto(request);
     }
 
-    private boolean usersExistInDb(RecommendationRequestDto recommendationRequest) {
+    private boolean usersExistInDb(RecommendationRequestDto recRequestDto) {
         boolean usersExist = recommendationRequestRepository.checkTheUsersExistInDb(
-                recommendationRequest.getRequesterId(), recommendationRequest.getReceiverId());
+                recRequestDto.getRequesterId(), recRequestDto.getReceiverId());
         if (!usersExist) {
             System.err.println("The users don't exist in database");
         }
@@ -58,13 +69,47 @@ public class RecommendationRequestService {
     }
 
     private boolean skillsExistInDb(RecommendationRequestDto recommendationRequest) {
-        return recommendationRequest.getSkills().stream()
-                .allMatch(skill -> skillRepository.existsByTitle(skill.getTitle()));
+        return skillRepository.countExisting(recommendationRequest
+                .getSkillsId()) == recommendationRequest.getSkillsId().size();
     }
 
     private boolean eligibleForRecommendation(RecommendationRequestDto recRequest) {
         return usersExistInDb(recRequest)
                 && sixMonthHavePassed(recRequest)
                 && skillsExistInDb(recRequest);
+    }
+
+    public List<RecommendationRequestDto> getRequests(RequestFilterDto filters) {
+        Stream<RecommendationRequest> requests = recommendationRequestRepository.findAll().stream();
+
+        for (RecommendationRequestFilter filter : recRequestFilters) {
+            if (filter.isApplicable(filters)) {
+                requests = filter.apply(requests, filters);
+            }
+        }
+
+        return requestMapper.toDtoList(requests.toList());
+    }
+
+    public RecommendationRequestDto getRequest(Long requestId) {
+        RecommendationRequest resultRequest = recommendationRequestRepository.findById(requestId)
+                .orElseThrow(() -> new NoSuchElementException("No request found by id: " + requestId));
+        return requestMapper.toDto(resultRequest);
+    }
+
+    public RecommendationRequestDto rejectRequest(long id, RejectionDto rejectionDto) {
+        if (rejectionDto.getReason() == null || rejectionDto.getReason().isBlank()) {
+            throw new IllegalArgumentException("Rejection reason not found");
+        }
+
+        RecommendationRequestDto recRequest = getRequest(id);
+
+        if (recRequest.getRejectionReason() == null || recRequest.getRejectionReason().isBlank()) {
+            recommendationRequestRepository.rejectRequest(id, rejectionDto.getReason());
+            recRequest.setRejectionReason(rejectionDto.getReason());
+        } else {
+            System.err.println("Request already rejected");
+        }
+        return recRequest;
     }
 }
---
+++ src/test/java/school/faang/user_service/service/RecommendationRequestServiceTest.java
@@ -0,0 +1,21 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+class RecommendationRequestServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getRequests() {
+    }
+
+    @Test
+    void getRequest() {
+    }
+
+    @Test
+    void rejectRequest() {
+    }
+}
