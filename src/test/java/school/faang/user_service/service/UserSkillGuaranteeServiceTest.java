98d317208940358aa2c82d1af58cbbf0445babea
Matvey <mashptz@gmail.com>
2024-11-09 19:29:07 +0300

    Исправление тестов

--- src/test/java/school/faang/user_service/service/UserSkillGuaranteeServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserSkillGuaranteeServiceTest.java
@@ -1,16 +1,15 @@
 package school.faang.user_service.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import school.faang.user_service.repository.UserSkillGuaranteeRepository;
 import school.faang.user_service.service.user_skill_guarantee.UserSkillGuaranteeService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,39 +23,27 @@ public class UserSkillGuaranteeServiceTest {
     private final long userId = 1L;
     private final long skillId = 101L;
 
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void testCreateGuarantee_GuaranteeDoesNotExist_CreatesNewGuarantee() {
-        when(userSkillGuaranteeRepository.existsByUserIdAndSkillId(userId, skillId)).thenReturn(false);
-        when(userSkillGuaranteeRepository.findMaxGuarantorId()).thenReturn(10L); // Максимальное значение гарантии — 10
-        when(userSkillGuaranteeRepository.create(userId, skillId, 11L)).thenReturn(11L); // Ожидаем создание новой с ID = 11
+        when(userSkillGuaranteeRepository.findMaxGuarantorId()).thenReturn(10L);
+        when(userSkillGuaranteeRepository.create(userId, skillId, 11L)).thenReturn(11L);
 
         Long result = userSkillGuaranteeService.createGuarantee(userId, skillId);
 
         assertNotNull(result);
         assertEquals(Long.valueOf(11L), result);
 
-        verify(userSkillGuaranteeRepository).existsByUserIdAndSkillId(userId, skillId);
         verify(userSkillGuaranteeRepository).findMaxGuarantorId();
         verify(userSkillGuaranteeRepository).create(userId, skillId, 11L);
     }
 
     @Test
-    public void testCreateGuarantee_GuaranteeAlreadyExists_ReturnsNull() {
-
-        when(userSkillGuaranteeRepository.existsByUserIdAndSkillId(userId, skillId)).thenReturn(true);
-
-        Long result = userSkillGuaranteeService.createGuarantee(userId, skillId);
-
-        assertNull(result);
-
-        verify(userSkillGuaranteeRepository).existsByUserIdAndSkillId(userId, skillId);
-        verify(userSkillGuaranteeRepository, never()).findMaxGuarantorId();
-        verify(userSkillGuaranteeRepository, never()).create(anyLong(), anyLong(), anyLong());
-    }
-
-    @Test
     public void testCreateGuarantee_MaxGuarantorIdIsNull_CreatesNewGuaranteeWithIdOne() {
-        when(userSkillGuaranteeRepository.existsByUserIdAndSkillId(userId, skillId)).thenReturn(false);
         when(userSkillGuaranteeRepository.findMaxGuarantorId()).thenReturn(null);
         when(userSkillGuaranteeRepository.create(userId, skillId, 1L)).thenReturn(1L);
 
@@ -65,7 +52,6 @@ public class UserSkillGuaranteeServiceTest {
         assertNotNull(result);
         assertEquals(Long.valueOf(1L), result);
 
-        verify(userSkillGuaranteeRepository).existsByUserIdAndSkillId(userId, skillId);
         verify(userSkillGuaranteeRepository).findMaxGuarantorId();
         verify(userSkillGuaranteeRepository).create(userId, skillId, 1L);
     }
