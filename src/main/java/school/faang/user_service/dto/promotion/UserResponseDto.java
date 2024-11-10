b18de2a3dbb28d10263299ddea8716c0e7dca8c1
UrmatBolotbek <urmar9797@gmail.com>
2024-11-10 02:01:05 +0100

    добавил новый ДТО для promotion

---
+++ src/main/java/school/faang/user_service/dto/promotion/UserResponseDto.java
@@ -0,0 +1,25 @@
+package school.faang.user_service.dto.promotion;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserResponseDto {
+    private long id;
+    private String username;
+    private String promotionTariff;
+    private Integer numberOfViews;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdAt;
+}
