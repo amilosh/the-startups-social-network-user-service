997cec528d3fe848badf44f3274022b3ad2276f4
Ivan Semikov <semikov.exf@gmail.com>
2024-11-10 09:45:27 +0300

    BJS2-40504 Request mapping for subscription controller

--- src/main/java/school/faang/user_service/controller/SubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -1,8 +1,14 @@
 package school.faang.user_service.controller;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.ShortUserDto;
 import school.faang.user_service.dto.UserFilterDto;
@@ -12,36 +18,43 @@ import school.faang.user_service.service.SubscriptionService;
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/subscription")
 @AllArgsConstructor
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
-    public ResponseEntity<Void> followUser(long followerId, long followeeId) {
+    @PostMapping("follow/{followerId}/to/{followeeId}")
+    public ResponseEntity<Void> followUser(@PathVariable @NotNull Long followerId, @PathVariable @NotNull Long followeeId) {
         subscriptionService.followUser(followerId, followeeId);
         return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<Void> unfollowUser(long followerId, long followeeId) {
+    @PostMapping("unfollow/{followerId}/to/{followeeId}")
+    public ResponseEntity<Void> unfollowUser(@PathVariable @NotNull Long followerId, @PathVariable @NotNull Long followeeId) {
         subscriptionService.unfollowUser(followerId, followeeId);
         return ResponseEntity.noContent().build();
     }
 
-    public ResponseEntity<List<ShortUserDto>> getFollowers(long followeeId, UserFilterDto filter) {
+    @PostMapping("followers/{followeeId}")
+    public ResponseEntity<List<ShortUserDto>> getFollowers(@PathVariable @NotNull Long followeeId, @RequestBody @NotNull UserFilterDto filter) {
         List<ShortUserDto> users = subscriptionService.getFollowers(followeeId, filter);
         return ResponseEntity.ok(users);
     }
 
-    public ResponseEntity<Long> getFollowersCount(long followerId) {
+    @GetMapping("followers-count/{followerId}")
+    public ResponseEntity<Long> getFollowersCount(@PathVariable @NotNull Long followerId) {
         long followersCount = subscriptionService.getFollowersCount(followerId);
         return ResponseEntity.ok(followersCount);
     }
 
-    public ResponseEntity<List<ShortUserDto>> getFollowing(long followerId, UserFilterDto filter) {
+    @PostMapping("following/{followerId}")
+    public ResponseEntity<List<ShortUserDto>> getFollowing(@PathVariable @NotNull Long followerId, UserFilterDto filter) {
         List<ShortUserDto> users = subscriptionService.getFollowing(followerId, filter);
         return ResponseEntity.ok(users);
     }
 
-    public ResponseEntity<Long> getFollowingCount(long followeeId) {
+    @GetMapping("following-count/{followeeId}")
+    public ResponseEntity<Long> getFollowingCount(@PathVariable @NotNull Long followeeId) {
         long followeeCount = subscriptionService.getFollowingCount(followeeId);
         return ResponseEntity.ok(followeeCount);
     }
