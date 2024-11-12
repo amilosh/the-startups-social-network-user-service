ee3bc140db8779f07d5fa30cd1e2ae8df6b7997f
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-12 17:58:59 +0300

    commit: conflict

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,17 +1,19 @@
 package school.faang.user_service.controller.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFilterDto;
 import school.faang.user_service.service.user.UserService;
 
 import java.util.List;
 
-@Controller
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
