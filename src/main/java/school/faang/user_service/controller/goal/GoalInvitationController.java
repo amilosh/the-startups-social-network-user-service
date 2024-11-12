fcb3c2d4394c8fde304bfe9cc6dd04ee6a41ac5c
Sergey Zhitenev <serzhitenev@yandex.ru>
2024-11-12 10:27:37 +0300

    feat: добавленны маппинг аннотации в GoalInvitationController

--- src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalInvitationController.java
@@ -5,8 +5,7 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.goal.GoalInvitationDto;
 import school.faang.user_service.dto.goal.InvitationFilterDto;
 import school.faang.user_service.service.GoalInvitationService;
@@ -14,27 +13,29 @@ import school.faang.user_service.service.GoalInvitationService;
 import java.util.List;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1/goalInvitation")
 @RequiredArgsConstructor
 @Validated
 public class GoalInvitationController {
     private final GoalInvitationService goalInvitationService;
 
+    @PostMapping("/create")
     public GoalInvitationDto createInvitation(GoalInvitationDto goalInvitationDto) {
         return goalInvitationService.createInvitation(goalInvitationDto);
     }
 
-    public GoalInvitationDto acceptInvitation(@NotNull @Positive long id) {
+    @PatchMapping("/accept/{id}")
+    public GoalInvitationDto acceptInvitation(@PathVariable @NotNull @Positive long id) {
         return goalInvitationService.acceptGoalInvitation(id);
     }
 
-    public GoalInvitationDto rejectInvitation(@NotNull @Positive long id) {
+    @PatchMapping("/reject/{id}")
+    public GoalInvitationDto rejectInvitation(@PathVariable @NotNull @Positive long id) {
         return goalInvitationService.rejectGoalInvitation(id);
     }
 
+    @PostMapping("/search")
     public List<GoalInvitationDto> getAllInvitations(InvitationFilterDto invitationFilterDto) {
         return goalInvitationService.getInvitations(invitationFilterDto);
     }
-
-
 }
