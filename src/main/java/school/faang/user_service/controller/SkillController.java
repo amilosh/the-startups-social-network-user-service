a17389181a415dc794ddd8ba288c9e2ec97c0f1c
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-11-12 11:20:11 +0300

    BJS2-40451

--- src/main/java/school/faang/user_service/controller/SkillController.java
+++ src/main/java/school/faang/user_service/controller/SkillController.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.service.SkillService;
@@ -9,27 +11,33 @@ import school.faang.user_service.validator.SkillValidator;
 
 import java.util.List;
 
-@Component
+@RestController
+@RequestMapping("/api/skills")
 @RequiredArgsConstructor
 public class SkillController {
     private final SkillService skillService;
     private final SkillValidator skillValidator;
 
-    public SkillDto create(SkillDto skillDto) {
+    @PostMapping
+    public ResponseEntity<SkillDto> create(@RequestBody SkillDto skillDto) {
         skillValidator.validateSkill(skillDto);
 
-        return skillService.create(skillDto);
+        SkillDto createdSkill = skillService.create(skillDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSkill);
     }
 
-    public List<SkillDto> getUserSkills(long userId) {
+    @GetMapping("/user/{userId}")
+    public List<SkillDto> getUserSkills(@PathVariable long userId) {
         return skillService.getUserSkills(userId);
     }
 
-    public List<SkillCandidateDto> getOfferedSkills(long userId) {
+    @GetMapping("/user/{userId}/offers")
+    public List<SkillCandidateDto> getOfferedSkills(@PathVariable long userId) {
         return skillService.getOfferedSkills(userId);
     }
 
-    public SkillDto acquireSkillFromOffers(long skillId, long userId) {
+    @PostMapping("/acquire")
+    public SkillDto acquireSkillFromOffers(@RequestParam long skillId, @RequestParam long userId) {
         return skillService.acquireSkillFromOffers(skillId, userId);
     }
 }
