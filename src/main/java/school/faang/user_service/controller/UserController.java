74cef833c5994d9d092086ee70d160e774625036
Vakarash <max.vackarash@gmail.com>
2024-11-12 12:26:37 +0300

    Небольшие изменения в контроллере с RequestMapping

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.service.UserService;
@@ -16,16 +17,17 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Validated
+@RequestMapping("/users")
 @RestController
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public UserDto getUser(@Positive @PathVariable long userId) {
         return userService.getUserDtoById(userId);
     }
 
-    @PostMapping("/users")
+    @PostMapping("/get")
     public List<UserDto> getUsersByIds(@NotEmpty @RequestBody List<@Positive Long> ids) {
         return userService.getAllUsersDtoByIds(ids);
     }
