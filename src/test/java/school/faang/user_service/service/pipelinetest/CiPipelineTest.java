f775181d9fae8d6e5a9b3bedd9a824c460fe80a7
daurenberdibekov <berder08@gmail.com>
2024-11-12 21:59:17 +0100

    test negative v2

--- src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
+++ src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
@@ -2,10 +2,10 @@ package school.faang.user_service.service.pipelinetest;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class CiPipelineTest {
-
     @Test
-    void pipelineFailTest(){
-
+    void testPipelineFail(){
     }
 }
