cd63b6b9c2ae16aa82acd55391f0a8e23d2ab4dc
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-13 19:25:23 +0300

    commit: controller

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -3,8 +3,14 @@ package school.faang.user_service.controller.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.dto.user.UserFilterDto;
 import school.faang.user_service.service.user.UserService;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,5 +22,9 @@ public class UserController {
     public void deactivateUser(@RequestParam Long userId) {
         userService.deactivateUser(userId);
         log.info("User with ID {} has been scheduled for the deactivation", userId);
+    }
+    @GetMapping(value = "premium")
+    public List<UserDto> getUsers(@ModelAttribute UserFilterDto filterDto) {
+        return userService.getUser(filterDto).toList();
     }
 }
