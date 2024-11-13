01ffc756d7b00a54184abed89c2f53ef706b6165
Alexey Zimin <zimin.alexey@gmail.com>
2024-11-13 23:56:54 +0300

    [BJS2-40445] - fixes + tests

--- src/main/java/school/faang/user_service/validator/recommendation/RecommendationValidator.java
+++ src/main/java/school/faang/user_service/validator/recommendation/RecommendationValidator.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.validator;
+package school.faang.user_service.validator.recommendation;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +16,10 @@ import java.util.stream.Stream;
 @Component
 @RequiredArgsConstructor
 public class RecommendationValidator {
-    public final static int PERIOD = 6;
+    public final static int MONTH_PERIOD = 6;
     public final static String CONTENT_IS_EMPTY = "Recommendation content is empty";
-    public final static String AUTHOR_NOT_FOUND = "Author with id = %s does not exist";
-    public final static String RECEIVER_NOT_FOUND = "Receiver with id = %s does not exist";
     public final static String SKILL_DOES_NOT_EXIST = "Skill with id = %s does not exist";
-    public final static String PERIOD_HAS_NOT_EXPIRED = "The author with id = %d made a recommendation to the user with id = %d less than 6 hours ago";
+    public final static String PERIOD_HAS_NOT_EXPIRED = "The author with id = %d made a recommendation to the user with id = %d less than " + MONTH_PERIOD + " hours ago";
     public final static String RECOMMENDATION_NOT_EXIST_BY_ID = "Recommendation with id = %s does not exist";
     public final static String RECOMMENDATION_NOT_EXIST = "Recommendation with author id = %s and receiver id = %s does not exist";
 
@@ -31,17 +29,16 @@ public class RecommendationValidator {
     private final UserValidator userValidator;
 
     public void validateContent(String content) {
-
         if (content == null || content.isBlank())
             throw new DataValidationException(CONTENT_IS_EMPTY);
     }
 
     public void validateAuthorExist(Long authorId) {
-        userValidator.validateUserExist(authorId, String.format(AUTHOR_NOT_FOUND, authorId));
+        userValidator.existsAuthorById(authorId);
     }
 
     public void validateReceiverExist(Long receiverId) {
-        userValidator.validateUserExist(receiverId, String.format(RECEIVER_NOT_FOUND, receiverId));
+        userValidator.existsReceiverById(receiverId);
     }
 
     public void validatePeriod(Long authorId, Long receiverId) {
@@ -49,10 +46,9 @@ public class RecommendationValidator {
                 recommendationRepository.findFirstByAuthorIdAndReceiverIdOrderByCreatedAtDesc(authorId, receiverId);
 
         recommendationOptional.ifPresent(r -> {
-            if (!r.getCreatedAt().isBefore(LocalDateTime.now().minusMonths(PERIOD))) {
+            if (!r.getCreatedAt().isBefore(LocalDateTime.now().minusMonths(MONTH_PERIOD))) {
                 String error = String.format(PERIOD_HAS_NOT_EXPIRED, authorId, receiverId);
-                log.error(error);
-                throw new IllegalArgumentException(error);
+                throw new DataValidationException(error);
             }
         });
     }
@@ -65,16 +61,14 @@ public class RecommendationValidator {
 
         recommendationOptional.ifPresent(r -> {
             String error = String.format(RECOMMENDATION_NOT_EXIST, recommendation.getAuthorId(), recommendation.getReceiverId());
-            log.error(error);
-            throw new IllegalArgumentException(error);
+            throw new DataValidationException(error);
         });
     }
 
     public void validateRecommendationExist(Long recommendationId) {
         if (!recommendationRepository.existsById(recommendationId)) {
             String error = String.format(RECOMMENDATION_NOT_EXIST_BY_ID, recommendationId);
-            log.error(error);
-            throw new IllegalArgumentException(error);
+            throw new DataValidationException(error);
         }
     }
 
@@ -82,8 +76,7 @@ public class RecommendationValidator {
         skillIds.forEach(skillId -> {
             if (!skillValidator.existsById(skillId)) {
                 String error = String.format(SKILL_DOES_NOT_EXIST, skillId);
-                log.error(error);
-                throw new IllegalArgumentException(error);
+                throw new DataValidationException(error);
             }
         });
     }
