0bc20a5808ffc591d9af05c79d623f1b41638587
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-13 22:04:01 +0300

    commit: GoalServiceTest

--- src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceTest.java
@@ -33,7 +33,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class GoalServiceTest {
+public class GoalServiceTest {
+
 
     @Mock
     private GoalRepository goalRepository;
