3690bbad0c51b16779c22fe71e485ef2fe1a1eca
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-11-13 08:41:49 +0100

    BJS2-40532 Организация события, code review fixes

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -1,44 +1,48 @@
 package school.faang.user_service.controller.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.event.EventDto;
 import school.faang.user_service.dto.event.EventFilterDto;
 import school.faang.user_service.service.event.EventService;
 
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/events")
+@Controller
+@RestController
+@RequestMapping("/events")
 public class EventController {
     private final EventService eventService;
 
-    //    @PostMapping("/event/create")
+    @PostMapping("/event/create")
     public EventDto create(EventDto event) {
         return eventService.create(event);
     }
 
-    //    @GetMapping("/{eventId}")
-    public EventDto getEvent(@PathVariable Long id) {
-        return eventService.getEvent(id);
+    @GetMapping("/{eventId}")
+    public EventDto getEvent(@PathVariable Long eventId) {
+        return eventService.getEvent(eventId);
     }
 
-    //    @GetMapping
+    @GetMapping
     public List<EventDto> getEventsByFilter(EventFilterDto filter) {
         return eventService.getEventsByFilter(filter);
     }
 
-    //    @GetMapping("/{ownerId}")
-    public List<EventDto> getOwnedEvents(@PathVariable Long userId) {
-        return eventService.getOwnedEvents(userId);
+    @GetMapping("/{ownerId}")
+    public List<EventDto> getOwnedEvents(@PathVariable Long ownerId) {
+        return eventService.getOwnedEvents(ownerId);
     }
 
-    //    @GetMapping("/{participantId}")
-    public List<EventDto> getParticipatedEvents(@PathVariable long userId) {
-        return eventService.getParticipatedEvents(userId);
+    @GetMapping("/{participantId}")
+    public List<EventDto> getParticipatedEvents(@PathVariable long participantId) {
+        return eventService.getParticipatedEvents(participantId);
     }
 
 }
