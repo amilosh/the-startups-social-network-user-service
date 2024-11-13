e336e87965ca26f7de06c093318064827d618c4d
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-11-13 13:02:07 +0500

    fix something in scheduler

--- src/main/java/school/faang/user_service/scheduler/premium/PremiumUpdateScheduler.java
+++ src/main/java/school/faang/user_service/scheduler/premium/PremiumUpdateScheduler.java
@@ -2,6 +2,8 @@ package school.faang.user_service.scheduler.premium;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.repository.premium.PremiumRepository;
@@ -10,12 +12,14 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
 public class PremiumUpdateScheduler {
 
     private final PremiumRepository premiumRepository;
 
-    @Scheduled(cron = "0 0-3 0 * * *", zone = "Europe/Moscow")
+    @Async
+    @Scheduled(cron = "0 0-2 * * * *", zone = "Europe/Moscow")
     void updatePremiumWhichExpired() {
         log.info("update premium scheduler working");
         premiumRepository.deletePremiumWhichDateExpire(LocalDateTime.now());
