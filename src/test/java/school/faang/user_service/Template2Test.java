c47c19e93d6b440ce35481dd7afe8ba0db330ce5
AlexLogino <alexeyjob3@gmail.com>
2024-11-13 13:55:49 +0300

    BJS2-40477 add TemplateTest

---
+++ src/test/java/school/faang/user_service/Template2Test.java
@@ -0,0 +1,13 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Template2Test {
+
+    @Test
+    public void testFail(){
+        fail();
+    }
+}
