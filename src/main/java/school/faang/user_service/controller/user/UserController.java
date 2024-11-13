2628aad656e6963f933ac19dbd65667cc382aaf1
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-11-13 05:38:44 +0300

    commit: Model Attribute

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -2,7 +2,7 @@ package school.faang.user_service.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.user.UserDto;
@@ -18,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping(value = "premium")
-    public List<UserDto> getUsers(@RequestBody UserFilterDto filterDto) {
+    public List<UserDto> getUsers(@ModelAttribute UserFilterDto filterDto) {
         return userService.getUser(filterDto).toList();
     }
 
