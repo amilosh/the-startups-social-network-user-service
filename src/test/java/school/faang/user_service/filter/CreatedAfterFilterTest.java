5e99dd63953def361d0cef8188a9ba30179fc9b2
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-11-15 02:55:33 +0300

    Modification: created CreatedBeforeFilterTest with methods: isFilterApplicableFieldIsPresent; isFilterApplicableFieldIsNotPresent; passedFilter; failedFilter;

---
+++ src/test/java/school/faang/user_service/filter/CreatedAfterFilterTest.java
@@ -0,0 +1,49 @@
+package school.faang.user_service.filter;
+
+import org.junit.jupiter.api.Test;
+import school.faang.user_service.dto.RequestFilterDto;
+import school.faang.user_service.entity.recommendation.RecommendationRequest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CreatedAfterFilterTest {
+    private final CreatedAfterFilter createdAfterFilter = new CreatedAfterFilter();
+
+    @Test
+    void isFilterApplicableFieldIsPresent() {
+        RequestFilterDto requestFilterDto = new RequestFilterDto()
+                .setCreatedAfter(LocalDateTime.now());
+        boolean filterApplicable = createdAfterFilter.isFilterApplicable(requestFilterDto);
+        assertTrue(filterApplicable);
+    }
+
+    @Test
+    void isFilterApplicableFieldIsNotPresent() {
+        RequestFilterDto requestFilterDto = new RequestFilterDto();
+        boolean filterApplicable = createdAfterFilter.isFilterApplicable(requestFilterDto);
+        assertFalse(filterApplicable);
+    }
+
+    @Test
+    void passedFilter(){
+        RecommendationRequest recommendationRequestDto = new RecommendationRequest()
+                .setCreatedAt(LocalDateTime.of(2024, 11, 15, 23, 35, 58));
+        RequestFilterDto requestFilterDto = new RequestFilterDto()
+                .setCreatedAfter(LocalDateTime.of(2024, 11, 14, 23, 35, 58));
+        boolean apply = createdAfterFilter.apply(recommendationRequestDto, requestFilterDto);
+        assertTrue(apply);
+    }
+
+    @Test
+    void failedFilter(){
+        RecommendationRequest recommendationRequestDto = new RecommendationRequest()
+                .setCreatedAt(LocalDateTime.of(2024, 11, 14, 23, 35, 58));
+        RequestFilterDto requestFilterDto = new RequestFilterDto()
+                .setCreatedAfter(LocalDateTime.of(2024, 11, 15, 23, 35, 58));
+        boolean apply = createdAfterFilter.apply(recommendationRequestDto, requestFilterDto);
+        assertFalse(apply);
+    }
+}
