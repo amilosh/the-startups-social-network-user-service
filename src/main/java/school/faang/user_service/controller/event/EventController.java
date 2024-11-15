1ae9d5b4f2b6ac725d0a5807a004dad9eab69269
Amanyaz Bayramdurdyyev <amanyaz9090@gmail.com>
2024-11-15 12:57:22 -0600

    Я выполнил задание BJS2-40538 без тестов. Задание было связано с событиями.

---
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -0,0 +1,88 @@
+package school.faang.user_service.controller.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.dto.event.EventFilterDto;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.service.event.EventService;
+
+import java.util.List;
+
+
+@Controller
+public class EventController {
+
+    private EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    public EventDto create(EventDto event) {
+        validateEvent(event);
+        return eventService.create(event);
+    }
+
+
+    public EventDto getEvent(Long id) {
+        return eventService.getEvent(id);
+    }
+
+    public List<EventDto> getEventsByFilter(EventFilterDto filter) {
+        return eventService.getEventsByFilter(filter);
+    }
+
+    public long deleteEvent(long eventId) {
+        return eventService.deleteEvent(eventId);
+    }
+
+    public EventDto updateEvent(EventDto event) {
+        validateEventUpdate(event);
+        return eventService.updateEvent(event);
+    }
+
+    public List<EventDto> getOwnedEvents(long userId) {
+        return eventService.getOwnedEvents(userId);
+    }
+
+    public List<EventDto> getParticipatedEvents(long userId) {
+        return eventService.getParticipatedEvents(userId);
+
+    }
+
+    public void validateEvent(EventDto event) {
+
+        if (event == null) {
+            throw new IllegalArgumentException("The event cannot be null.");
+        }
+        if (event.getTitle().isBlank() || event.getTitle() == null) {
+            throw new IllegalArgumentException("The title cannot be null or empty.");
+        }
+        if (event.getStartDate() == null) {
+            throw new IllegalArgumentException("The start date is required.");
+        }
+        if (event.getOwnerId() == null) {
+            throw new IllegalArgumentException("The event must have an associated user (ownerId)");
+        }
+
+    }
+
+    public void validateEventUpdate(EventDto event) {
+        if (event == null) {
+            throw new DataValidationException("This event doesn't exist!");
+        }
+        if (event.getTitle().isBlank()) {
+            throw new DataValidationException("This event doesn't have the title!");
+        }
+        if (event.getStartDate().equals(null)) {
+            throw new DataValidationException("This event doesn't have start date!");
+        }
+        if (event.getOwnerId().equals(null)) {
+            throw new DataValidationException("This eent does't have an owner!");
+        }
+    }
+}
+
+
---
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -0,0 +1,100 @@
+package school.faang.user_service.service.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.event.EventDto;
+import school.faang.user_service.dto.event.EventFilterDto;
+import school.faang.user_service.dto.skill.SkillDto;
+import school.faang.user_service.entity.Skill;
+import school.faang.user_service.entity.event.Event;
+import school.faang.user_service.mapper.event.EventMapper;
+import school.faang.user_service.repository.SkillRepository;
+import school.faang.user_service.repository.event.EventRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
+
+public class EventService {
+
+    private final EventRepository eventRepository;
+    private final SkillRepository skillRepository;
+    private final EventMapper eventMapper;
+
+    @Autowired
+    public EventService(EventRepository eventRepository, SkillRepository skillRepository, EventMapper eventMapper) {
+        this.eventRepository = eventRepository;
+        this.skillRepository = skillRepository;
+        this.eventMapper = eventMapper;
+    }
+
+
+    public EventDto create(EventDto eventDto) {
+        if (!hasRequiredSkills(eventDto)) {
+            throw new IllegalArgumentException("User does not have the required skills to conduct this event");
+        }
+        Event event = eventMapper.toEntity(eventDto);
+        Event savedEvent = eventRepository.save(event);
+        return eventMapper.toDto(savedEvent);
+    }
+
+    private boolean hasRequiredSkills(EventDto event) {
+        List<Long> requiredSkillIds = event.getRelatedSkills().stream().map(SkillDto::getId).collect(Collectors.toList());
+        List<Long> ownerSkillIds = skillRepository.findAllByUserId(event.getOwnerId()).stream().map(Skill::getId).collect(Collectors.toList());
+        return ownerSkillIds.containsAll(requiredSkillIds);
+    }
+
+    public EventDto getEvent(long eventId) {
+        Optional<Event> foundEvent = eventRepository.findById(eventId);
+        if (foundEvent.isPresent()) {
+            Event gotEvent = foundEvent.get();
+            return eventMapper.toDto(gotEvent);
+        } else throw new IllegalArgumentException("This event is not exist");
+    }
+
+    public List<EventDto> getEventsByFilter(EventFilterDto filter) {
+        List<Event> events = eventRepository.findAll();
+
+
+        return events.stream()
+                .filter(event -> filter.getTitle() == null || event.getTitle().contains(filter.getTitle()))
+                .filter(event -> filter.getStartDateFrom() == null || !event.getStartDate().isBefore(filter.getStartDateFrom().atStartOfDay()))
+                .filter(event -> filter.getStartDateTo() == null || !event.getStartDate().isAfter(filter.getStartDateTo().atStartOfDay()))
+                .filter(event -> filter.getOwnerId() == null || event.getOwner().getId() == (filter.getOwnerId()))
+                .map(eventMapper::toDto)
+                .collect(Collectors.toList());
+
+
+    }
+
+    public long deleteEvent(long eventId) {
+        eventRepository.deleteById(eventId);
+        return eventId;
+    }
+
+    public EventDto updateEvent(EventDto event) {
+        if (!hasRequiredSkills(event)) {
+            throw new IllegalArgumentException("The user cannot update the event, since doesn't have the required skills");
+        }
+        Event eventEntity = eventMapper.toEntity(event);
+        Event savedEvent = eventRepository.save(eventEntity);
+        return eventMapper.toDto(savedEvent);
+
+    }
+
+    public List<EventDto> getOwnedEvents(long userId) {
+        List<Event> events = eventRepository.findAllByUserId(userId);
+        return eventMapper.toDtoList(events);
+    }
+
+    public List<EventDto> getParticipatedEvents(long userId) {
+        List<Event> events = eventRepository.findParticipatedEventsByUserId(userId);
+        return eventMapper.toDtoList(events);
+    }
+
+}
+
+
