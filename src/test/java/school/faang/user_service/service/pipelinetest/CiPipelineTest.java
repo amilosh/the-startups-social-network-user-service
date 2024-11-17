953c71af8f3cc390cc15175567425b66d76e8aae
daurenberdibekov <berder08@gmail.com>
2024-11-17 14:43:40 +0100

    updated test positive for checkstyle v1

--- src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
+++ src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.service.pipelinetest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.web.bind.annotation.RestController;
 
 class CiPipelineTest {
     @Test
