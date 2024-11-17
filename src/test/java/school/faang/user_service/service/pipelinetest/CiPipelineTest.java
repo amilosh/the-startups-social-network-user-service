3f60917873800c2ee35bc0b8969b2327240b24ae
daurenberdibekov <berder08@gmail.com>
2024-11-17 13:10:45 +0100

    test ci checkstyle negative v1

--- src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
+++ src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
@@ -1,11 +1,15 @@
 package school.faang.user_service.service.pipelinetest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 class CiPipelineTest {
     @Test
     void testCiFail(){
     }
+
+
 }
