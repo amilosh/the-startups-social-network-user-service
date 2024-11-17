42aa1b868b0c1f9add6c6c43d896c20d0d4fcdc3
Sanasar1 <almasyansb@gmail.com>
2024-11-17 23:35:49 +0300

    добавил логи

--- src/main/java/school/faang/user_service/service/MentorshipService.java
+++ src/main/java/school/faang/user_service/service/MentorshipService.java
@@ -3,6 +3,7 @@ package school.faang.user_service.service;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
@@ -13,12 +14,15 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MentorshipService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     @Transactional
     public void deleteMentee(long menteeId, long mentorId) {
+        log.info("Trying to delete mentee: {} for mentor: {}",
+                menteeId, mentorId);
         User mentor = userRepository.findById(mentorId)
                 .orElseThrow(() -> new EntityNotFoundException("Mentor not found"));
 
@@ -32,6 +36,8 @@ public class MentorshipService {
 
     @Transactional
     public void deleteMentor(long menteeId, long mentorId) {
+        log.info("Trying to delete mentor: {} for mentee: {}",
+                mentorId, menteeId);
         User mentee = userRepository.findById(menteeId)
                 .orElseThrow(() -> new EntityNotFoundException("Mentee not found"));
 
@@ -45,17 +51,21 @@ public class MentorshipService {
 
     @Transactional
     public List<UserDto> getMentees(long userId) {
+        log.info("Trying to get list of mentees for user: {}",
+                userId);
         return userMapper.toDto(
                 userRepository.findById(userId)
-                .map(User::getMentees)
-                .orElseThrow(() -> new EntityNotFoundException("Mentor not found")));
+                        .map(User::getMentees)
+                        .orElseThrow(() -> new EntityNotFoundException("Mentor not found")));
     }
 
     @Transactional
     public List<UserDto> getMentors(long userId) {
+        log.info("Trying to get list of mentors for user: {}",
+                userId);
         return userMapper.toDto(
                 userRepository.findById(userId)
-                .map(User::getMentors)
-                .orElseThrow(() -> new EntityNotFoundException("Mentee not found")));
+                        .map(User::getMentors)
+                        .orElseThrow(() -> new EntityNotFoundException("Mentee not found")));
     }
 }
