bb4bcd0b3ed1babaf922017770b09bc68f799f6f
daurenberdibekov <berder08@gmail.com>
2024-11-17 14:36:25 +0100

    test ci checkstyle v1

--- src/main/java/school/faang/user_service/controller/SkillController.java
+++ src/main/java/school/faang/user_service/controller/SkillController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
