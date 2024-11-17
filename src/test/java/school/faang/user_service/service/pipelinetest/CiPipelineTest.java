df22d9355395a05cbd1187cb45a2155f69df20bb
daurenberdibekov <berder08@gmail.com>
2024-11-17 14:37:57 +0100

    updated test negative v1

--- src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
+++ src/test/java/school/faang/user_service/service/pipelinetest/CiPipelineTest.java
@@ -3,8 +3,6 @@ package school.faang.user_service.service.pipelinetest;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 class CiPipelineTest {
     @Test
     void testCiFail(){
