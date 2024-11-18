6ec7e802766686dc8e6ffaeccbd8dcefdf57ec99
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-11-18 20:05:30 +0300

    entityExceptionHandler

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,6 +30,13 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDto handlerIllegalArgumentException(IllegalArgumentException e) {
         log.error("Illegal Argument Exception", e);
+        return new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto handlerEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity Not Found Exception", e);
         return new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
