08545c98d3c848803d9b55e695bde4844d06a63a
Irina Krasnova <arisha.17@mail.ru>
2024-11-19 10:01:20 +0300

    add RestController && MentorshipControllerTest  add message&log

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -27,35 +27,49 @@ public class MentorshipController {
 
     @GetMapping("users/{userId}/mentees")
     public ResponseEntity<List<UserDto>> getMentees(
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "UserId must be greater than 0 ")
             long userId) {
+        log.info("Getting list of user mentees with id {} ", userId);
+
         return ResponseEntity.ok(mentorshipService.getMentees(userId));
     }
 
     @GetMapping("users/{userId}/mentors")
     public ResponseEntity<List<UserDto>> getMentors(
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "UserId must be greater than 0 ")
             long userId) {
+        log.info("Getting list of user mentors with id {} ", userId);
+
         return ResponseEntity.ok(mentorshipService.getMentors(userId));
     }
 
     @DeleteMapping("/mentors/{mentorId}/mentees/{menteeId}")
     public ResponseEntity<Void> deleteMentee(
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "MentorId must be greater than 0 ")
             long mentorId,
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "MenteeId must be greater than 0 ")
             long menteeId) {
         mentorshipService.deleteMentee(mentorId, menteeId);
+        log.info("Removing a mentee with  Id {} from the list of a mentor with Id {} ", menteeId, mentorId);
+
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/mentees/{menteeId}/mentors/{mentorId}")
     public ResponseEntity<Void> deleteMentor(
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "MenteeId must be greater than 0 ")
             long menteeId,
-            @PathVariable @NotNull @Positive
+            @PathVariable @NotNull(message = "id field is required ")
+            @Positive(message = "MentorId must be greater than 0 ")
             long mentorId) {
         mentorshipService.deleteMentor(menteeId, mentorId);
+        log.info("Removing a mentor with  Id {} from the list of a mentee with Id {} ", mentorId, menteeId);
+
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
