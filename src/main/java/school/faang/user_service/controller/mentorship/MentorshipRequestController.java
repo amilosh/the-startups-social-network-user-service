0e4211ddad6869ad9dc88908ad4646681f9d5f23
UrmatBolotbek <urmar9797@gmail.com>
2024-11-20 17:22:27 +0100

    добавил валидацию и  исправил мелкие ошибки во всем что касается Mentorship

--- src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipRequestController.java
@@ -3,7 +3,10 @@ package school.faang.user_service.controller.mentorship;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,63 +18,58 @@ import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.mentorship.MentorshipRequestDto;
 import school.faang.user_service.dto.mentorship.RejectionDto;
 import school.faang.user_service.dto.mentorship.RequestFilterDto;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.mentorship.MentorshipRequestService;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/mentorship-request")
-@Tag(name = "Mentorship Request", description = "Контроллер для запроса на менторство")
-@ApiResponse(description = "Успешная отработка метода", responseCode = "200")
-@ApiResponse(description = "Ошибка клиента", responseCode = "400")
-@ApiResponse(description = "Ошибка сервера", responseCode = "500")
+@Tag(name = "Mentorship Request", description = "Controller for managing mentorship requests")
+@ApiResponse(description = "Successful method execution", responseCode = "200")
+@ApiResponse(description = "Client error", responseCode = "400")
+@ApiResponse(description = "Server error", responseCode = "500")
 public class MentorshipRequestController {
 
     private final MentorshipRequestService mentorshipRequestService;
 
     @Operation(
-            summary = "Запрос на менторство",
-            description = "Отправка запроса на менторство другому пользователю"
+            summary = "Request mentorship",
+            description = "Send a mentorship request to another user"
     )
     @PostMapping
-    public void requestMentorship(@RequestBody MentorshipRequestDto mentorshipRequestDto) {
-        validation(mentorshipRequestDto);
+    public void requestMentorship(@Valid @RequestBody MentorshipRequestDto mentorshipRequestDto) {
         mentorshipRequestService.requestMentorship(mentorshipRequestDto);
     }
 
     @Operation(
-            summary = "Получение всех запросов на менторство",
-            description = "Получение всех запросов на менторство используя фильтры"
+            summary = "Get all mentorship requests",
+            description = "Retrieve all mentorship requests using filters"
     )
     @GetMapping
-    public List<MentorshipRequestDto> getRequests(@ModelAttribute RequestFilterDto filter) {
+    public List<MentorshipRequestDto> getRequests(@Valid @ModelAttribute RequestFilterDto filter) {
         return mentorshipRequestService.getRequests(filter);
     }
 
     @Operation(
-            summary = "Принять запрос на менторство",
-            description = "Возможность принимать запрос на менторство, пришедший от другого пользователя"
+            summary = "Accept mentorship request",
+            description = "Allow a user to accept a mentorship request from another user"
     )
     @PutMapping("/accept/{mentorshipRequestId}")
-    public void acceptRequest(@PathVariable long mentorshipRequestId) {
+    public void acceptRequest(
+            @PathVariable @NotNull(message = "Mentorship request ID should not be null") Long mentorshipRequestId) {
         mentorshipRequestService.acceptRequest(mentorshipRequestId);
     }
 
     @Operation(
-            summary = "Отклонить запрос на менторство",
-            description = "Возможность пользователя отклонять запрос на менторство"
+            summary = "Reject mentorship request",
+            description = "Allow a user to reject a mentorship request"
     )
     @PutMapping("/reject/{mentorshipRequestId}")
-    public void rejectRequest(@PathVariable long mentorshipRequestId,@RequestBody RejectionDto rejection) {
+    public void rejectRequest(
+            @PathVariable @NotNull(message = "Mentorship request ID should not be null") Long mentorshipRequestId,
+            @Valid @RequestBody RejectionDto rejection) {
         mentorshipRequestService.rejectRequest(mentorshipRequestId, rejection);
     }
-
-    private void validation(MentorshipRequestDto recommendationDto) {
-        if (recommendationDto.getDescription() == null || recommendationDto.getDescription().isBlank()) {
-            throw new DataValidationException("A request for mentorship must contain a reason");
-        }
-    }
-
 }
