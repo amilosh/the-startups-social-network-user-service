9c25754f58d91b36238b11b54da0962521a78c13
Юрий <angelbes777@gmail.com>
2024-11-20 19:06:04 +0300

    Fix minio config

--- src/main/java/school/faang/user_service/config/minio/MinioConfig.java
+++ src/main/java/school/faang/user_service/config/minio/MinioConfig.java
@@ -1,10 +1,16 @@
 package school.faang.user_service.config.minio;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.SetBucketPolicyArgs;
+import io.minio.errors.MinioException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class MinioConfig {
 
@@ -17,11 +23,54 @@ public class MinioConfig {
     @Value("${minio.secretKey}")
     private String secretKey;
 
+    @Value("${minio.publicPolicy}")
+    private String publicPolicy;
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
     @Bean
     public MinioClient minioClient() {
-        return MinioClient.builder()
+        MinioClient client = MinioClient.builder()
                 .endpoint(minioUrl)
                 .credentials(accessKey, secretKey)
                 .build();
+
+        createBucketIfNotExists(client);
+        applyPublicPolicy(client);
+
+        return client;
+    }
+
+    private void createBucketIfNotExists(MinioClient client) {
+        try {
+            boolean bucketExists = client.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!bucketExists) {
+                client.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                log.info("Bucket created: {}", bucketName);
+            } else {
+                log.info("Bucket already exists: {}", bucketName);
+            }
+        } catch (MinioException e) {
+            log.info("Error creating bucket: {}", e.getMessage());
+        } catch (Exception e) {
+            log.info("Error: {}", e.getMessage());
+        }
+    }
+
+    private void applyPublicPolicy(MinioClient client) {
+        try {
+            client.setBucketPolicy(
+                    SetBucketPolicyArgs.builder()
+                            .bucket(bucketName)
+                            .config(publicPolicy)
+                            .build()
+            );
+            log.info("Public policy applied to bucket: {}", bucketName);
+        } catch (MinioException e) {
+            log.info("Error applying public policy: {}", e.getMessage());
+        } catch (Exception e) {
+            log.info("Error: {}", e.getMessage());
+        }
     }
 }
