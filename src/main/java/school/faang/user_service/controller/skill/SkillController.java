bf256f1fe2378d3227ecc5688fcfe80bd52709ff
UrmatBolotbek <urmar9797@gmail.com>
2024-11-20 19:31:28 +0100

    добавил валидацию в Skill

--- src/main/java/school/faang/user_service/controller/skill/SkillController.java
+++ src/main/java/school/faang/user_service/controller/skill/SkillController.java
@@ -1,17 +1,22 @@
 package school.faang.user_service.controller.skill;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.service.skill.SkillService;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/skills")
@@ -19,30 +24,26 @@ public class SkillController {
     private final SkillService service;
 
     @PostMapping
-    public SkillDto create(@RequestBody SkillDto skillDto) {
-        validateSkill(skillDto);
+    public SkillDto create(@Valid @RequestBody SkillDto skillDto) {
         return service.create(skillDto);
     }
 
-    public List<SkillDto> getUserSkills(long userId) {
+    @GetMapping("/users/{userId}")
+    public List<SkillDto> getUserSkills(
+            @PathVariable @NotNull(message = "User ID should not be null") Long userId) {
         return service.getUserSkills(userId);
     }
 
-    public List<SkillCandidateDto> getOfferedSkills(long userId) {
+    @GetMapping("/users/{userId}/offered-skills")
+    public List<SkillCandidateDto> getOfferedSkills(
+            @PathVariable @NotNull(message = "User ID should not be null") Long userId) {
         return service.getOfferedSkills(userId);
     }
 
-    public SkillDto acquireSkillFromOffers(long skillId, long userId) {
+    @PostMapping("/users/{userId}/acquire/{skillId}")
+    public SkillDto acquireSkillFromOffers(
+            @PathVariable @NotNull(message = "Skill ID should not be null") Long skillId,
+            @PathVariable @NotNull(message = "User ID should not be null") Long userId) {
         return service.acquireSkillFromOffers(skillId, userId);
-    }
-
-    private void validateSkill(SkillDto skillDto) {
-        if (skillDto == null || skillDto.getTitle() == null || skillDto.getTitle().isBlank()) {
-            throw new DataValidationException("""
-                    Validation failed. Check next:
-                    -Skill not equal null
-                    -Skill title not equal null or empty"""
-            );
-        }
     }
 }
--- src/main/java/school/faang/user_service/dto/skill/SkillDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillDto.java
@@ -1,5 +1,8 @@
 package school.faang.user_service.dto.skill;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SkillDto {
     private Long id;
+
+    @NotBlank(message = "The skill title should not be blank")
+    @NotEmpty(message = "The skill title should not be empty")
+    @Size(min = 1, max = 64, message = "The skill title should be between 1 and 64 characters long")
     private String title;
 }
