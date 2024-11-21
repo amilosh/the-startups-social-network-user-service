bbefa07155b8bc1185b1ed7b0c0255321ada0662
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-11-21 16:19:36 +0300

    completed task Like System and added UserController

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,10 +1,17 @@
 package school.faang.user_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private long id;
     private String username;
@@ -12,4 +19,6 @@ public class UserDto {
     private String email;
     private List<Long> menteeIds;
     private List<Long> mentorIds;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/school/faang/user_service/service/user/UserService.java
+++ src/main/java/school/faang/user_service/service/user/UserService.java
@@ -2,7 +2,10 @@ package school.faang.user_service.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
 import java.util.Optional;
@@ -11,8 +14,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public Optional<User> findById(long userId) {
         return userRepository.findById(userId);
+    }
+
+    public UserDto getUser(long userId) {
+        User user = findById(userId).orElseThrow(
+                () -> new DataValidationException("User not found"));
+        return userMapper.toDto(user);
     }
 }
