023a02b62d7093e637b19bc0b6e662273252f68a
beksultanramankul <ramankul.beksultan.ramankul@gmail.com>
2024-11-22 01:37:12 +0500

    first

---
+++ src/main/java/school/faang/user_service/dto/RecommendationReceivedEvent.java
@@ -0,0 +1,16 @@
+package school.faang.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecommendationReceivedEvent {
+    private Long recommendationId;
+    private Long requesterId;
+    private Long receiverId;
+}
