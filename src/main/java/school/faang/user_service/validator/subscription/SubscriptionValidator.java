fe5e7c1401c2e01a88a633fc0ff96503010e4919
zlomilk95 <kenobi_95@mail.ru>
2024-11-24 19:02:38 +0300

    BJS2-43494 ref

--- src/main/java/school/faang/user_service/validator/subscription/SubscriptionValidator.java
+++ src/main/java/school/faang/user_service/validator/subscription/SubscriptionValidator.java
@@ -1,23 +1,27 @@
 package school.faang.user_service.validator.subscription;
 
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.SubscriptionRepository;
 
-@Data
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class SubscriptionValidator {
     private final SubscriptionRepository subscriptionRepository;
 
     public void validateUserIsTryingToCallHimself(long followerId, long followeeId) {
         if (followerId == followeeId) {
+            log.error("User {} tries to perform an action on himself", followeeId);
             throw new DataValidationException("User " + followeeId + " tries to perform an action on himself");
         }
     }
 
     public void validateUserAlreadyHasThisSubscription(long followerId, long followeeId) {
         if (subscriptionRepository.existsByFollowerIdAndFolloweeId(followerId, followeeId)) {
+            log.error("User {} already has such a subscription", followeeId);
             throw new DataValidationException("User " + followeeId + " already has such a subscription");
         }
     }
