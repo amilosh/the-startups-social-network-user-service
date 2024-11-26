fccaaf438b6e50373466b8d87731dee00f845b87
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-11-26 20:51:57 +0300

    BJS2-41288 ban user user service

--- src/test/java/school/faang/user_service/service/UserServiceTest.java
+++ src/test/java/school/faang/user_service/service/UserServiceTest.java
@@ -9,7 +9,7 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.mapper.user.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
 import java.util.Optional;
