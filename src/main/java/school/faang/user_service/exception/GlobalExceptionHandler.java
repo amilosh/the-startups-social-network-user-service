51c0addc8f138d9a3cb37996703a7fb9ce59ef33
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-11-29 16:35:15 +0500

    deleted unused import

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -6,10 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.naming.SizeLimitExceededException;
