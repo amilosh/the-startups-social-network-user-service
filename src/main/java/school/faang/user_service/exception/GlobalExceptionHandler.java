189c95b179ec56ed0de3fa658f5aba3254b5037f
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-11-29 15:14:50 +0500

    solved conflicts

--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -3,18 +3,15 @@ package school.faang.user_service.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.SizeLimitExceededException;
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import javax.naming.SizeLimitExceededException;
 
 @Slf4j
 @RestControllerAdvice
@@ -59,6 +56,18 @@ public class GlobalExceptionHandler {
         return buildResponse(ex);
     }
 
+    @ExceptionHandler(SizeLimitExceededException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleSizeLimitExceededException(SizeLimitExceededException ex) {
+        return buildResponse(ex);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return buildResponse(ex);
+    }
+
     private ErrorResponse buildResponse(Exception ex) {
         log.error(ex.getClass().getName(), ex);
         return ErrorResponse.builder()
@@ -66,27 +75,5 @@ public class GlobalExceptionHandler {
                 .error(ex.getClass().getName())
                 .message(Objects.requireNonNullElse(ex.getMessage(), "No message available"))
                 .build();
-    public ErrorResponse handleDataValidationException(DataValidationException ex) {
-        return new ErrorResponse("DATA_VALIDATION_ERROR", ex.getMessage());
     }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ErrorResponse("INVALID_ARGUMENT", ex.getMessage());
-    }
-
-    @ExceptionHandler(SizeLimitExceededException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleSizeLimitExceededException(SizeLimitExceededException ex) {
-        return new ErrorResponse("SIZE_LIMIT_EXCEEDED", ex.getMessage());
-    }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleDataIntegrityViolationException() {
-        String message = "Data in the file is not unique, please upload file with correct data.";
-        return new ErrorResponse("DATA_INTEGRITY_VIOLATION", message);
-    }
-
 }
