53263b26b0b302f074e94a659ec2fa7eeea3ef57
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-11-29 14:48:37 +0500

    refactored

---
+++ src/main/java/school/faang/user_service/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package school.faang.user_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String errorCode;
+    private String message;
+}
--- src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package school.faang.user_service.exception;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,27 +13,27 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleDataValidationException(DataValidationException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ErrorResponse handleDataValidationException(DataValidationException ex) {
+        return new ErrorResponse("DATA_VALIDATION_ERROR", ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ErrorResponse("INVALID_ARGUMENT", ex.getMessage());
     }
 
     @ExceptionHandler(SizeLimitExceededException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleSizeLimitExceededException(SizeLimitExceededException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ErrorResponse handleSizeLimitExceededException(SizeLimitExceededException ex) {
+        return new ErrorResponse("SIZE_LIMIT_EXCEEDED", ex.getMessage());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleDataIntegrityViolationException() {
-        String message = "Data in the file is not unique, please upload file with correct data : ";
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    public ErrorResponse handleDataIntegrityViolationException() {
+        String message = "Data in the file is not unique, please upload file with correct data.";
+        return new ErrorResponse("DATA_INTEGRITY_VIOLATION", message);
     }
 
 }
