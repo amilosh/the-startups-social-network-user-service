b81189127b92a62653cb800baf39c373d9f581ab
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-11-30 21:21:34 +0300

    bug fix

--- src/main/java/school/faang/user_service/controller/premium/PremiumController.java
+++ src/main/java/school/faang/user_service/controller/premium/PremiumController.java
@@ -2,11 +2,14 @@ package school.faang.user_service.controller.premium;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.premium.PremiumDto;
+import school.faang.user_service.entity.PremiumPeriod;
 import school.faang.user_service.service.premium.PremiumService;
 
 import java.util.List;
@@ -25,5 +28,9 @@ public class PremiumController {
     @PutMapping("/updatePremium")
     public List<PremiumDto> updatePremium(@RequestBody List<PremiumDto> premiums){
         return premiumService.updatePremium(premiums);
+    }
+    @PostMapping("/{days}")
+    public PremiumDto buyPremium(@PathVariable int days) {
+        return premiumService.buyPremium(PremiumPeriod.fromDays(days));
     }
 }
--- src/test/java/school/faang/user_service/service/premium/PremiumServiceTest.java
+++ src/test/java/school/faang/user_service/service/premium/PremiumServiceTest.java
@@ -7,17 +7,29 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import school.faang.user_service.client.PaymentServiceClient;
+import school.faang.user_service.config.context.UserContext;
+import school.faang.user_service.dto.client.Currency;
+import school.faang.user_service.dto.client.PaymentResponse;
+import school.faang.user_service.dto.client.PaymentStatus;
 import school.faang.user_service.dto.premium.PremiumDto;
+import school.faang.user_service.entity.PremiumPeriod;
+import school.faang.user_service.entity.User;
 import school.faang.user_service.entity.premium.Premium;
 import school.faang.user_service.mapper.premium.PremiumMapperImpl;
 import school.faang.user_service.repository.premium.PremiumRepository;
+import school.faang.user_service.service.UserService;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -30,7 +42,18 @@ class PremiumServiceTest {
     private PremiumMapperImpl premiumMapper;
 
     @Mock
+    private UserService userService;
+
+    @Mock
+    private UserContext userContext;
+
+    @Mock
     private PremiumRepository premiumRepository;
+
+    @Mock
+    private PaymentServiceClient paymentServiceClient;
+    @Mock
+    ResponseEntity<PaymentResponse> response;
 
     private PremiumDto firstPremium;
 
@@ -84,5 +107,54 @@ class PremiumServiceTest {
         assertEquals(premiumDtos.size(), 1);
         assertEquals(result.getId(), firstPremium.getId());
         assertEquals(result.isActive(), firstPremium.isActive());
+    }
+
+    @Test
+    public void testUserAlreadyHasPremium() {
+        setupMocks(true);
+
+        assertThrows(IllegalStateException.class,
+                () -> premiumService.buyPremium(PremiumPeriod.YEAR));
+
+    }
+
+    @Test
+    public void testResponseBodyIsNull() {
+        setupMocks(false);
+
+        when(paymentServiceClient.sendPayment(any())).thenReturn(response);
+        when(response.getBody()).thenReturn(null);
+
+        assertThrows(RestClientException.class,
+                () -> premiumService.buyPremium(PremiumPeriod.YEAR));
+    }
+
+    @Test
+    public void testBuyPremiumSuccessful() {
+        PaymentResponse paymentResponse = new PaymentResponse(
+                PaymentStatus.SUCCESS,
+                0,
+                0,
+                BigDecimal.valueOf(1),
+                Currency.USD,
+                "message"
+        );
+
+        setupMocks(false);
+
+        when(paymentServiceClient.sendPayment(any())).thenReturn(response);
+        when(response.getBody()).thenReturn(paymentResponse);
+        when(userService.getUserById(1L)).thenReturn(new User());
+
+        premiumService.buyPremium(PremiumPeriod.YEAR);
+
+        verify(premiumRepository).save(any());
+    }
+
+    private void setupMocks(boolean existsByUserId) {
+        long userId = 1L;
+
+        when(userContext.getUserId()).thenReturn(userId);
+        when(premiumRepository.existsByUserId(userId)).thenReturn(existsByUserId);
     }
 }
