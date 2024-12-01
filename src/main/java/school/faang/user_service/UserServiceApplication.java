55855c95db9a32e05af212293692cca502fdcd18
UrmatBolotbek <urmar9797@gmail.com>
2024-12-02 00:05:21 +0100

    закончил задание "Удаление прошедших событий"

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
@@ -23,6 +26,9 @@ import org.springframework.context.annotation.Bean;
                 )
         )
 )
+@EnableScheduling
+@EnableAsync
+@EnableRetry
 public class UserServiceApplication {
 
     public static void main(String[] args) {
