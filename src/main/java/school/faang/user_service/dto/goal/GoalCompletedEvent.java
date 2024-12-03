0e97f6f4431264aadb468e492c73963756c0bdc2
Roma Nifontov <romannif@mail.ru>
2024-12-03 12:28:12 +0500

    Промежуточный коммит

--- src/main/java/school/faang/user_service/dto/goal/GoalCompletedEvent.java
+++ src/main/java/school/faang/user_service/dto/goal/GoalCompletedEvent.java
@@ -1,10 +1,12 @@
 package school.faang.user_service.dto.goal;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class GoalCompletedEvent {
     private long goalId;
     private long userId;
--- src/main/java/school/faang/user_service/publisher/goal/GoalCompletedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/goal/GoalCompletedEventPublisher.java
@@ -1,6 +1,10 @@
 package school.faang.user_service.publisher.goal;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.goal.GoalCompletedEvent;
@@ -8,16 +12,21 @@ import school.faang.user_service.entity.goal.Goal;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class GoalCompletedEventPublisher {
 
     private RedisTemplate<String, Object> redisTemplate;
+    private ObjectMapper objectMapper;
 
     public void publish(GoalCompletedEvent event) {
-        redisTemplate.convertAndSend("goal_completed_topic", event);
+        try {
+            String json = objectMapper.writeValueAsString(event);
+            redisTemplate.convertAndSend("goal_completed_topic", json);
+        } catch (JsonProcessingException e) {
+            log.error("An error occurred while working with JSON: ", e);
+            throw new RuntimeException(e);
+        }
     }
-
-
-
 
 
 }
