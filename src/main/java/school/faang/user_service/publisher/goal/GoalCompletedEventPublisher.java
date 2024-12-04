2c6d4d1287a0f81a971db551023f26a72017b4ac
Roma Nifontov <romannif@mail.ru>
2024-12-04 12:31:53 +0500

    Сделал изменения в задаче BJS2-41388

--- src/main/java/school/faang/user_service/publisher/goal/GoalCompletedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/goal/GoalCompletedEventPublisher.java
@@ -1,19 +1,21 @@
 package school.faang.user_service.publisher.goal;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.goal.GoalCompletedEvent;
-import school.faang.user_service.entity.goal.Goal;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class GoalCompletedEventPublisher {
+
+    @Value("${spring.data.redis.channels.goal-completed-channel.name}")
+    private String topicGoalCompleted;
 
     private RedisTemplate<String, Object> redisTemplate;
     private ObjectMapper objectMapper;
@@ -21,7 +23,7 @@ public class GoalCompletedEventPublisher {
     public void publish(GoalCompletedEvent event) {
         try {
             String json = objectMapper.writeValueAsString(event);
-            redisTemplate.convertAndSend("goal_completed_topic", json);
+            redisTemplate.convertAndSend(topicGoalCompleted, json);
         } catch (JsonProcessingException e) {
             log.error("An error occurred while working with JSON: ", e);
             throw new RuntimeException(e);
