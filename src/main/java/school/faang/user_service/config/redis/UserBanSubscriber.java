d5b9edc1941eed0f5e4184201b9078ce2025d4ef
Юрий <angelbes777@gmail.com>
2024-12-05 22:41:50 +0300

    Fix

--- src/main/java/school/faang/user_service/config/redis/UserBanSubscriber.java
+++ src/main/java/school/faang/user_service/config/redis/UserBanSubscriber.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.config.redis;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
