a32eb5e0c6dfdb4a11c5063b19c8a0096db97d7c
Ilya Kryuchkov <ilya222268@gmail.com>
2024-12-05 23:59:37 +0200

    BJS2-40755 - Добавить тест на проверку того, что Spring Context успешно поднимается

---
+++ src/test/java/school/faang/user_service/ApplicationContextTest.java
@@ -0,0 +1,11 @@
+package school.faang.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationContextTest {
+
+    @Test
+    void checkContext() {}
+}
