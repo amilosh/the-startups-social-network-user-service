74ebcd7d4c5f726621af038e23ff4919342a1d6c
Andrew <andrew@MPro.local>
2024-12-05 17:03:25 +0700

    Feature / Notification Service: User Service Publisher
    Redis config

---
+++ src/main/java/school/faang/user_service/config/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package school.faang.user_service.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(
+                redisProperties.getHost(),
+                redisProperties.getPort()
+        );
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
+}
