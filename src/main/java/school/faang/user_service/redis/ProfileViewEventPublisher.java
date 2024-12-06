c7a5968fbb8f286d79c65ec1bd13f3fc73645b02
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-12-06 18:23:24 +0600

    refactored

--- src/main/java/school/faang/user_service/redis/ProfileViewEventPublisher.java
+++ src/main/java/school/faang/user_service/redis/ProfileViewEventPublisher.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.events.ProfileViewEvent;
@@ -8,10 +9,12 @@ import school.faang.user_service.dto.events.ProfileViewEvent;
 @Component
 @RequiredArgsConstructor
 public class ProfileViewEventPublisher{
+    @Value("${spring.data.redis.channels.profile-view-channel.name}")
+    private String profileView;
 
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void publish(ProfileViewEvent event) {
-        redisTemplate.convertAndSend("profile-view", event);
+        redisTemplate.convertAndSend(profileView, event);
     }
 }
