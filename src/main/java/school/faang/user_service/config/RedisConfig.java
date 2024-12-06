01369bdf79ada75c6efd66991f1ca27cd0d1a6f5
SerUKalinin <serukalinin@gmail.com>
2024-12-06 11:47:12 +0300

    RedisConfig

---
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -0,0 +1,70 @@
+package school.faang.user_service.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.channels.follower-event-channel.follower}")
+    private String followerEventChannel;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        log.info("Создание LettuceConnectionFactory для Redis с хостом: {} и портом:{}", redisHost, redisPort);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        log.info("LettuceConnectionFactory успешно создан");
+        return connectionFactory;
+    }
+
+    @Bean
+    public ChannelTopic followerChannel() {
+        log.info("Создание ChannelTopic для канала: {}", followerEventChannel);
+        return new ChannelTopic(followerEventChannel);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("Создание RedisTemplate с кастомными сериализаторами.");
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(serializer);
+
+        log.info("RedisTemplate создан и сериализаторы настроены.");
+
+        return redisTemplate;
+    }
+
+
+}
