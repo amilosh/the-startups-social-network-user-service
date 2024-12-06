f77e1567b35b5cfdb499cdc2e17e3f415721c246
bogda <bogdanskrip404@gmail.com>
2024-12-06 20:20:31 +0500

    исправил путь топика, доработал конфиг редиса

--- src/main/java/school/faang/user_service/publisher/RecommendationEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/RecommendationEventPublisher.java
@@ -17,7 +17,7 @@ public class RecommendationEventPublisher {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
 
-    @Value("${spring.data.redis.channels.recommendation-topic}")
+    @Value("${spring.data.redis.channels.recommendation_topic}")
     private String channel;
 
     public void publish(RecommendationEvent event)  {
